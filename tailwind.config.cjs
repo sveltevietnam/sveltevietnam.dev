e81d29eaffff9bc834ccec9378894cc6a0037437
Quang Phan <phan.quang@4digit.jp>
2023-02-13 15:57:39 +0700

    chore: setup color pallete & header height var

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const postcss = require('postcss');
 const plugin = require('tailwindcss/plugin');
 
 const sveltevietnam = plugin(
-  ({ addVariant }) => {
+  ({ addVariant, addComponents, theme }) => {
     addVariant('data-current', '&[data-current]:not([data-current="false"])');
     addVariant('aria-current', '&[aria-current]:not([aria-current="false"])');
     addVariant('dark', [
@@ -23,6 +23,16 @@ const sveltevietnam = plugin(
         container.append(mediaRule);
       },
     ]);
+
+    addComponents({
+      '.c-container': {
+        'max-width': '1200px',
+        'padding-left': theme('spacing.8'),
+        'padding-right': theme('spacing.8'),
+        'margin-left': 'auto',
+        'margin-right': 'auto',
+      },
+    });
   },
   {
     theme: {
@@ -32,6 +42,43 @@ const sveltevietnam = plugin(
         },
         fontFamily: {
           inter: ['Inter', 'sans-serif'],
+        },
+        spacing: {
+          header: 'var(--header-height)',
+        },
+        colors: {
+          status: {
+            info: '#3ABFF8',
+            success: '#36D399',
+            warning: '#FBBD23',
+            error: '#F87272',
+          },
+          grayscale: {
+            light: {
+              100: '#ffffff',
+              200: '#E5E5E5',
+              300: '#999999',
+              400: '#4C4C4C',
+            },
+            dark: {
+              100: '#1C1C1C',
+              200: '#585757',
+              300: '#969696',
+              400: '#E8E8E8',
+            },
+          },
+          fg: {
+            100: 'var(--color-fg-100)',
+            200: 'var(--color-fg-200)',
+            300: 'var(--color-fg-300)',
+            400: 'var(--color-fg-400)',
+          },
+          bg: {
+            100: 'var(--color-bg-100)',
+            200: 'var(--color-bg-200)',
+            300: 'var(--color-bg-300)',
+            400: 'var(--color-bg-400)',
+          },
         },
       },
     },
