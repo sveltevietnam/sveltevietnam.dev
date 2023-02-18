3def89b7162b5696dbb10c19205f46cdd89da751
Quang Phan <vnphanquang@gmail.com>
2023-02-18 17:56:15 +0700

    feat(page.sponsor): bootstrap & wireframe

--- src/lib/shared/services/i18n/translations/pages/sponsor/en.ts
+++ src/lib/shared/services/i18n/translations/pages/sponsor/en.ts
@@ -1,3 +1,21 @@
+import { EMAILS } from '$shared/constants';
+
 import type { vi } from './vi';
 
-export const en = {} satisfies typeof vi;
+export const en = {
+  title: 'Sponsor',
+  intro:
+    'Svelte Vietnam chooses <a class="c-link font-bold" href="https://docs.opencollective.com/help/about/introduction" target="_blank" rel="noreferrer">Open Collective</a> to receive financial contributions from sponsors (individuals or organizations) thanks to its great support for open source and the transparency it provides: all donations and expenses are recorded and public to everyone.',
+  ctas: {
+    sponsor: 'Sponsor through Open Collective',
+    question: `Send questions to <em>${EMAILS.sponsor}</em>`,
+  },
+  why: {
+    title: 'Why does Svelte Vietnam need sponsorship?',
+    description:
+      'Svelte Vietnam is made possible by a collective effort from many individuals: maintaining infrastructure and site content, organizing events, helping others in forums, contributing to open source and nonprofit projects, ... Many of these people have their own full-time job and family. Financial sponsorship will help reduce unavoidable costs and create an incentive to sustain this valuable effort.<br/><br/>Apart from financial donation, individuals and organizations can support in organizing events, providing space for meetups, and spreading not only the adoption of Svelte, but also the open source mindset and sharing habit within the technology community in Vietnam.',
+  },
+  sponsors: {
+    cta: 'See list of sponsors and financial report at Open Collective',
+  },
+} satisfies typeof vi;
--- src/lib/shared/services/i18n/translations/pages/sponsor/vi.ts
+++ src/lib/shared/services/i18n/translations/pages/sponsor/vi.ts
@@ -1,1 +1,19 @@
-export const vi = {};
+import { EMAILS } from '$shared/constants';
+
+export const vi = {
+  title: 'Tài trợ',
+  intro:
+    'Svelte Vietnam chọn <a class="c-link font-bold" href="https://docs.opencollective.com/help/about/introduction" target="_blank" rel="noreferrer">Open Collective</a> để nhận đóng góp tài chính từ nhà tài trợ (cá nhân hoặc tổ chức) vì nền tảng này hỗ trợ tốt cho các dự án open source, đồng thời cung cấp sự minh bạch trong quản lý tài chính: mọi đóng góp (tiền vào) và chi tiêu (tiền ra) đều được lưu trữ và hiển thị công khai đến tất cả mọi người.',
+  ctas: {
+    sponsor: 'Tài trợ thông qua Open Collective',
+    question: `Gởi thắc mắc đến <em>${EMAILS.sponsor}</em>`,
+  },
+  why: {
+    title: 'Vì sao Svelte Vietnam cần tài trợ?',
+    description:
+      'Svelte Vietnam được hiện thực hoá bằng nhiều nỗ lực cá nhân: duy trì hạ tầng và nội dung trang web, tổ chức sự kiện, chia sẻ qua diễn đàn, đóng góp vào các dự án open source, phi lợi nhuận, ... và thường đến từ những thành viên có công việc và gia đình riêng. Hỗ trợ tài chính sẽ giúp giảm bớt các chi phí không tránh khỏi và tạo động lực cho những nỗ lực mang nhiều giá trị như vậy.<br/><br/>Ngoài tài chính, cá nhân và tổ chức có điều kiện còn có thể hỗ trợ trong việc tổ chức, cung cấp địa điểm cho các sự kiện, buổi gặp mặt của Svelte Vietnam, và cùng lan truyền tư duy open source và thói quen chia sẻ trong cộng đồng công nghệ tại Việt Nam.',
+  },
+  sponsors: {
+    cta: 'Xem báo cáo tài chính và danh sách nhà tài trợ tại Open Collective',
+  },
+};
