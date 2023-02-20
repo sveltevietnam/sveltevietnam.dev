476780ea5c62b3c659451387d31360f3e80080d9
Quang Phan <phan.quang@4digit.jp>
2023-02-20 11:35:04 +0700

    chore: set site as under construction, hide all content for now

--- src/routes/[[lang=lang]]/+layout.svelte
+++ src/routes/[[lang=lang]]/+layout.svelte
@@ -1,34 +1,3 @@
-<script lang="ts">
-  import { invalidate } from '$app/navigation';
-  import { Footer } from '$client/components/organisms/Footer';
-  import { Header } from '$client/components/organisms/Header';
-  import { LOAD_DEPENDENCIES } from '$shared/constants';
-  import type { ColorScheme } from '$shared/types';
-
-  import type { PageData } from './$types';
-
-  export let data: PageData;
-
-  $: clientColorScheme = data.colorScheme;
-  async function changeColorScheme(e: CustomEvent<ColorScheme>) {
-    const scheme = e.detail;
-    if (clientColorScheme === scheme) return;
-    clientColorScheme = scheme;
-    document.documentElement.dataset.colorScheme = scheme;
-    await fetch('/api/color-scheme', {
-      method: 'POST',
-      body: scheme,
-    });
-    invalidate(LOAD_DEPENDENCIES.COLOR_SCHEME);
-  }
-</script>
-
-<Header
-  lang={data.language}
-  colorScheme={clientColorScheme}
-  on:colorSchemeChange={changeColorScheme}
-/>
-
-<slot />
-
-<Footer lang={data.language} />
+<main class="grid h-screen place-items-center text-4xl">
+  Site under construction. Check back soon!
+</main>
