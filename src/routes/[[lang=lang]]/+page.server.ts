e5b6f9ce79c308b861108ca92867504fd6c2c8c7
Quang Phan <vnphanquang@gmail.com>
2023-03-18 18:10:12 +0700

    refactor(pages): migrate translations location to adjacent

--- src/routes/[[lang=lang]]/+page.server.ts
+++ src/routes/[[lang=lang]]/+page.server.ts
@@ -1,8 +1,8 @@
 import { createMockedEvents, createMockedJobs, createMockedProjects } from '$shared/mocks';
 import type { Language } from '$shared/services/i18n';
-import { translations } from '$shared/services/i18n/translations/pages/home';
 
 import type { PageServerLoadEvent } from './$types';
+import { translations } from './_page/translation';
 
 export async function load({ params }: PageServerLoadEvent) {
   const lang = params.lang as Language;
--- src/routes/[[lang=lang]]/events/+page.server.ts
+++ src/routes/[[lang=lang]]/events/+page.server.ts
@@ -2,9 +2,9 @@ import { mail } from '$shared/actions/mail/mail.server';
 import { translations as mailT } from '$shared/actions/mail/translation';
 import { createMockedEvents } from '$shared/mocks';
 import type { Language } from '$shared/services/i18n';
-import { translations as pageT } from '$shared/services/i18n/translations/pages/events';
 
 import type { PageServerLoadEvent, Actions } from './$types';
+import { translations as pageT } from './_page/translation';
 
 export async function load({ params }: PageServerLoadEvent) {
   const lang = params.lang as Language;
