47388c464cca19d3d3a1ebb9d75579a4b44c4f04
Quang Phan <vnphanquang@gmail.com>
2023-03-18 17:04:45 +0700

    feat(client.sevice.cache): implement base cache service

---
+++ src/lib/client/services/cache/browser-cache.decorator.ts
@@ -0,0 +1,41 @@
+import { browser } from '$app/environment';
+
+import type { BrowserCacheServiceConstructor } from './browser-cache.class';
+
+export type BrowserCacheDecoratorParams<Data> = {
+  key: string;
+  storage: 'local' | 'session' | Storage;
+  defaultCache: Data;
+};
+
+// FIXME: current strategy relies on Stage 2 decorators proposal
+// and the Typescript --experimentalDecorators flag
+// Keep an eye for docs on https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/#decorators
+// and refactor to Stage 3 implementation when it's stable
+export function BrowserCache<Data>(params: BrowserCacheDecoratorParams<Data>) {
+  const { key, storage, defaultCache } = params;
+
+  return function <C extends BrowserCacheServiceConstructor<Data>>(constructor: C) {
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+
+        if (browser) {
+          if (defaultCache && this.get() === null) {
+            this.set(defaultCache);
+          }
+        }
+      }
+
+      public get key() {
+        return key;
+      }
+
+      public get storage() {
+        if (storage === 'local') return localStorage;
+        else if (storage === 'session') return sessionStorage;
+        return storage;
+      }
+    };
+  };
+}
---
+++ src/lib/client/services/cache/index.ts
@@ -0,0 +1,2 @@
+export * from './browser-cache.class';
+export * from './browser-cache.decorator';
