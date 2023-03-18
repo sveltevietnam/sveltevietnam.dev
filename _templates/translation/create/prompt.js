f2d30df31467473473628e9b61ba1e18b1223bd1
Quang Phan <vnphanquang@gmail.com>
2023-03-18 19:57:00 +0700

    refactor(hygen.translation): support creating translation file relatively at each domain location

--- _templates/translation/create/prompt.js
+++ _templates/translation/create/prompt.js
@@ -1,33 +1,80 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { readdirSync } = require('fs');
+const path = require('path');
+
+const glob = require('tiny-glob/sync');
+
+const getDirectories = (source) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => ({
+      path: `${source}/${dirent.name}`,
+      name: dirent.name,
+    }));
+
+const CONFIG = {
+  page: {
+    base: 'src/routes',
+    getDirs() {
+      const pages = glob(`${CONFIG.page.base}/**/+page.svelte`).sort();
+      return pages.map((p) => {
+        const dirname = path.dirname(p);
+        let name = path.relative(CONFIG.page.base, dirname.replace('[[lang=lang]]', ''));
+        if (name === '') name = 'home';
+        const level = name.split('/').length;
+        return {
+          name: `${'    '.repeat(level - 1)}${name}`,
+          path: path.join(dirname, '_page'),
+        };
+      });
+    },
+  },
+  action: {
+    base: 'src/lib/shared/actions',
+    getDirs() {
+      return getDirectories(CONFIG.action.base);
+    },
+  },
+  component: {
+    base: 'src/lib/client/components',
+    getDirs() {
+      return getDirectories(CONFIG.component.base);
+    },
+  },
+  modal: {
+    base: 'src/lib/client/modals',
+    getDirs() {
+      return getDirectories(CONFIG.modal.base);
+    },
+  },
+};
+
 // see types of prompts:
 // https://github.com/enquirer/enquirer/tree/master/examples
 module.exports = {
   prompt: async ({ prompter, args }) => {
-    let name = args.name;
-    if (!name) {
-      ({ name } = await prompter.prompt({
-        type: 'input',
-        name: 'name',
-        message: 'Give it a name:',
-        required: true,
-      }));
-      name = name
-        .toLowerCase()
-        // replace non letter/numbers with a hyphen
-        .replace(/[^a-zA-Z0-9]/g, '-');
-    }
+    let tPath = args.path;
 
-    let domain = args.domain;
-    if (!domain) {
-      ({ domain } = await prompter.prompt({
+    if (!tPath) {
+      const { domain } = await prompter.prompt({
         type: 'select',
         name: 'domain',
-        message: 'Pick a domain:',
+        message: 'Which domain to create translation for?',
         choices: ['page', 'action', 'component', 'modal'],
         required: true,
-      }));
-      domain = `${domain}s/`;
+      });
+      const { getDirs } = CONFIG[domain];
+      const dirs = getDirs();
+      const { target } = await prompter.prompt({
+        type: 'select',
+        name: 'target',
+        message: 'Which target to create translation for?',
+        choices: dirs.map((d) => d.name),
+        required: true,
+      });
+      const dir = dirs.find((d) => d.name === target);
+      tPath = dir.path;
     }
-
-    return { name, domain };
+    return { path: tPath };
   },
 };
--- _templates/translation/create/vi.ejs.t
+++ _templates/translation/create/vi.ejs.t
@@ -1,5 +1,5 @@
 ---
-to: src/lib/shared/services/i18n/translations/<%=domain%><%=name%>/vi.ts
+to: <%=path%>/translation/vi.ts
 unless_exists: true
 ---
 export const vi = {};
