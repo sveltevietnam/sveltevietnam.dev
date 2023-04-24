b1d0cacc2d08f65940c0741b4f1ac4c1d60bea68
Quang Phan <vnphanquang@gmail.com>
2023-04-24 08:07:42 +0700

    style(components.AnimatedArrowCircle): reduce motion of hover animation

--- src/lib/client/components/AnimatedArrowCircle/AnimatedArrowCircle.svelte
+++ src/lib/client/components/AnimatedArrowCircle/AnimatedArrowCircle.svelte
@@ -58,7 +58,7 @@
   class={cls}
   bind:this={node}
 >
-  <circle cx="32" cy="32" r="24" fill="currentColor" class="circle" />
+  <circle cx="32" cy="32" r="24" fill="none" stroke="currentcolor" />
   <path
     d="M32 56C45.2548 56 56 45.2548 56 32C56 18.7452 45.2548 8 32 8C18.7452 8 8 18.7452 8 32C8 45.2548 18.7452 56 32 56Z"
     stroke="currentcolor"
@@ -84,37 +84,22 @@
 
 <style lang="postcss">
   svg {
-    --animation-duration: 400ms;
+    --animation-duration: 650ms;
     --animation-timing-function: cubic-bezier(0.13, -0.27, 0.48, 1.54);
     --animated-color: currentcolor;
 
     transition: color 200ms ease-out;
 
-    & .circle,
-    & .arrow {
-      animation-duration: var(--animation-duration);
-      animation-fill-mode: forwards;
-    }
-
-    & .circle {
-      transform-origin: center;
-      animation-name: circle-animate-out;
-      animation-duration: calc(var(--animation-duration));
-      animation-timing-function: var(--animation-timing-function);
-    }
-
-    & .arrow {
+    .arrow {
       animation-name: arrow-animate-out;
+      animation-duration: var(--animation-duration);
       animation-timing-function: var(--animation-timing-function);
+      animation-fill-mode: forwards;
     }
   }
 
   svg[data-animated='true'] {
     color: var(--animated-color);
-
-    & .circle {
-      animation-name: circle-animate-in;
-    }
 
     & .arrow {
       animation-name: arrow-animate-in;
@@ -126,59 +111,29 @@
       color: var(--animated-color);
     }
 
-    & .circle {
-      animation-name: circle-animate-in;
-    }
-
     & .arrow {
       animation-name: arrow-animate-in;
-    }
-  }
-
-  @keyframes circle-animate-in {
-    from {
-      transform: scale(0);
-      opacity: 0;
-    }
-
-    to {
-      transform: scale(1);
-      opacity: 1;
-    }
-  }
-
-  @keyframes circle-animate-out {
-    from {
-      opacity: 1;
-    }
-
-    to {
-      opacity: 0;
     }
   }
 
   @keyframes arrow-animate-in {
     0% {
       transform: translate(0);
-      color: theme('colors.design.fg.1');
       opacity: 1;
     }
 
     50% {
       transform: translate(100%);
-      color: theme('colors.design.fg.1');
       opacity: 0;
     }
 
     51% {
       transform: translate(-100%);
-      color: theme('colors.design.bg.1');
       opacity: 0;
     }
 
     100% {
       transform: translate(0);
-      color: theme('colors.design.bg.1');
       opacity: 1;
     }
   }
@@ -186,25 +141,21 @@
   @keyframes arrow-animate-out {
     0% {
       transform: translate(0);
-      color: theme('colors.design.bg.1');
       opacity: 1;
     }
 
     50% {
       transform: translate(100%);
-      color: theme('colors.design.bg.1');
       opacity: 0;
     }
 
     51% {
       transform: translate(-100%);
-      color: theme('colors.design.fg.1');
       opacity: 0;
     }
 
     100% {
       transform: translate(0);
-      color: theme('colors.design.fg.1');
       opacity: 1;
     }
   }
