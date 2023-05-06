297ea87770248c026a665a4866798405a7a91600
Quang Phan <vnphanquang@gmail.com>
2023-05-06 18:30:00 +0700

    refactor: correctly declare build info global constants & add to root layout data

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,19 +6,20 @@
 import type { Language } from '$shared/services/i18n';
 import type { ColorScheme } from '$shared/types';
 
-declare const __BUILD_TIMESTAMP__: string;
-declare const __BUILD_HASH__: string;
-declare const __BUILD_VERSION__: string;
-
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
 declare global {
+  declare const __BUILD_TIMESTAMP__: string;
+  declare const __BUILD_HASH__: string;
+  declare const __BUILD_VERSION__: string;
+
   namespace App {
     interface Locals {
       userId: string;
       colorScheme: ColorScheme;
       language: Language;
+      version: string;
     }
     interface PageData {
       colorScheme: ColorScheme;
