c2372659d9ca7bd40a7fa654418dc9ddd62471c8
Quang Phan <vnphanquang@gmail.com>
2023-05-06 09:48:41 +0700

    feat(home.intro): add intro & scrolling animation

--- src/routes/[[lang=lang]]/_page/components/SectionIntro.svelte
+++ src/routes/[[lang=lang]]/_page/components/SectionIntro.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import embla from 'embla-carousel-svelte';
+  import gsap from 'gsap';
+  import { Power1 } from 'gsap';
+  import { onMount } from 'svelte';
 
   import { intersect } from '$client/actions/intersect';
+  import { splash } from '$client/components/SplashScreen';
   import type { Language } from '$shared/services/i18n';
 
   import introShapeEllipse from '../images/intro-shape-ellipse.png';
@@ -16,16 +20,212 @@
   export let lang: Language;
 
   $: t = translations[lang].intro;
+
+  let sectionElement: HTMLElement;
+  let titleElement: HTMLElement;
+  // card elements
+  let cardsContainerElement: HTMLElement;
+  let svelteCardElement: HTMLElement;
+  let vietnamCardElement: HTMLElement;
+  let sveltevietnamCardElement: HTMLElement;
+  $: cardElements = [svelteCardElement, vietnamCardElement, sveltevietnamCardElement];
+  // shape elements
+  let backdropElement: HTMLElement;
+  let starElement: HTMLElement;
+  let triangleLargeElement: HTMLElement;
+  let triangleSmallElement: HTMLElement;
+  let ellipseElement: HTMLElement;
+  $: shapeElements = [starElement, triangleLargeElement, triangleSmallElement, ellipseElement];
+
+  let intersected = false;
+
+  let introTimeline: gsap.core.Timeline;
+  let scrollTimeline: gsap.core.Timeline;
+  let parallaxTimeline: gsap.core.Timeline;
+  onMount(async () => {
+    const ScrollTrigger = (await import('gsap/ScrollTrigger')).ScrollTrigger;
+    gsap.registerPlugin(ScrollTrigger);
+
+    // TODO: improve responsive animation with https://greensock.com/docs/v3/GSAP/gsap.matchMedia()
+
+    // title & cards
+    gsap.set([titleElement, ...cardElements], { opacity: 0, y: 80 });
+    // shapes
+    gsap.set(shapeElements, {
+      x: (_, target: HTMLElement) => gsap.getProperty(target, '--initial-translate-x').toString(),
+      y: (_, target: HTMLElement) => gsap.getProperty(target, '--initial-translate-y').toString(),
+      rotateZ: (_, target: HTMLElement) => gsap.getProperty(target, '--rotate-z-from').toString(),
+      opacity: 0,
+    });
+    gsap.set(sectionElement, { opacity: 1 });
+
+    parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: sectionElement,
+        start: 'top+=10 top',
+        end: 'bottom center',
+        scrub: 0.1,
+        invalidateOnRefresh: true,
+        markers: true,
+      },
+      paused: true,
+    });
+    parallaxTimeline
+      .fromTo(
+        svelteCardElement,
+        {
+          y: 0,
+        },
+        {
+          y: (_, target: HTMLElement) =>
+            (target.parentElement?.offsetHeight ?? 0) - target.offsetHeight,
+        },
+        0,
+      )
+      .fromTo(
+        sveltevietnamCardElement,
+        {
+          y: 0,
+        },
+        {
+          y: (_, target: HTMLElement) =>
+            target.offsetHeight - (target.parentElement?.offsetHeight ?? 0),
+        },
+        0,
+      );
+
+    scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        toggleActions: 'play none reverse none',
+        start: 20,
+        end: '+=1',
+        invalidateOnRefresh: true,
+        markers: true,
+      },
+      paused: true,
+      defaults: {
+        duration: 1,
+        ease: Power1.easeInOut,
+      },
+    });
+
+    function getY() {
+      const { top, height } = cardsContainerElement.getBoundingClientRect();
+      const viewBoxHeight = window.innerHeight;
+      if (top < viewBoxHeight / 2) {
+        return -80;
+      }
+      const y = top - height / 2 - viewBoxHeight / 2;
+      return y;
+    }
+
+    scrollTimeline
+      .to(
+        sectionElement,
+        {
+          marginTop: getY,
+        },
+        0,
+      )
+      .to(
+        titleElement,
+        {
+          y: () => -0.5 * getY(),
+          opacity: 0.1,
+        },
+        0,
+      )
+      .to(
+        backdropElement,
+        {
+          y: () => -0.5 * getY(),
+          opacity: 0.1,
+        },
+        0,
+      )
+      .to(
+        shapeElements,
+        {
+          x: (_, target: HTMLElement) =>
+            gsap.getProperty(target, '--initial-translate-x').toString(),
+          y: (_, target: HTMLElement) =>
+            gsap.getProperty(target, '--initial-translate-y').toString(),
+        },
+        0,
+      );
+
+    introTimeline = gsap.timeline({
+      defaults: {
+        ease: Power1.easeOut,
+        duration: 0.8,
+      },
+      onComplete() {
+        parallaxTimeline.play();
+        scrollTimeline.play();
+      },
+      paused: true,
+    });
+
+    // title & cards
+    introTimeline.to(
+      [titleElement, ...cardElements],
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.2,
+      },
+      0,
+    );
+
+    // shapes
+    introTimeline
+      .to(
+        shapeElements,
+        {
+          opacity: 1,
+          x: 0,
+          y: 0,
+          stagger: 0.15,
+        },
+        0,
+      )
+      .to(
+        shapeElements,
+        {
+          rotateX: (_, target: HTMLElement) => gsap.getProperty(target, '--rotate-x').toString(),
+          rotateY: (_, target: HTMLElement) => gsap.getProperty(target, '--rotate-y').toString(),
+          rotateZ: (_, target: HTMLElement) => gsap.getProperty(target, '--rotate-z-to').toString(),
+          scale: (_, target: HTMLElement) => gsap.getProperty(target, '--scale').toString(),
+          repeat: -1,
+          yoyo: true,
+          duration: 3,
+          ease: Power1.easeInOut,
+          stagger: 0.1,
+        },
+        '>',
+      );
+    return () => {
+      introTimeline?.kill();
+      scrollTimeline?.kill();
+      parallaxTimeline?.kill();
+    };
+  });
+
+  $: if (intersected && $splash?.done && introTimeline) {
+    introTimeline.play();
+  }
 </script>
 
 <section
   class="intro"
-  use:intersect={{
-    class: false,
-    intersectedClass: 'intersected',
-  }}
+  style="opacity: 0"
+  class:intersected
+  use:intersect={{ class: false, intersectedClass: false }}
+  on:intersect:once={() => (intersected = true)}
+  bind:this={sectionElement}
 >
-  <h1 title="Svelte Vietnam" class="intro-title">
+  <h1 title="Svelte Vietnam" class="intro-title" bind:this={titleElement}>
     <svg inline-src="../images/intro-title" width="824" height="301" />
   </h1>
   <div
@@ -41,9 +241,10 @@
       },
       plugins: [],
     }}
+    bind:this={cardsContainerElement}
   >
     <ul class="embla__container">
-      <li class="embla__slide intro-card intro-card--svelte">
+      <li class="embla__slide intro-card intro-card--svelte" bind:this={svelteCardElement}>
         <article>
           <img src={introSvelteImg} alt="swirling winged-shaped star" width="60" height="60" />
           <h2>Svelte</h2>
@@ -51,7 +252,7 @@
           <p>{t.svelte}</p>
         </article>
       </li>
-      <li class="embla__slide intro-card intro-card--vietnam">
+      <li class="embla__slide intro-card intro-card--vietnam" bind:this={vietnamCardElement}>
         <article>
           <img src={introVietnamImg} alt="five-pointed star" width="60" height="60" />
           <h2>Vietnam</h2>
@@ -59,7 +260,10 @@
           <p>{t.vietnam}</p>
         </article>
       </li>
-      <li class="embla__slide intro-card intro-card--sveltevietnam">
+      <li
+        class="embla__slide intro-card intro-card--sveltevietnam"
+        bind:this={sveltevietnamCardElement}
+      >
         <article>
           <img src={introSvelteVietnamImg} alt="eight-pointed start" width="60" height="60" />
           <h2>Svelte Vietnam</h2>
@@ -69,15 +273,30 @@
       </li>
     </ul>
   </div>
-  <div aria-disabled class="intro-backdrop">
-    <img src={introShapeStar} alt="ellipse" width="174" height="174" class="star" />
-    <img src={introShapeEllipse} alt="ellipse" width="317" height="505" class="ellipse" />
+  <div aria-disabled class="intro-backdrop" bind:this={backdropElement}>
+    <img
+      src={introShapeStar}
+      alt="star"
+      width="174"
+      height="174"
+      class="star"
+      bind:this={starElement}
+    />
+    <img
+      src={introShapeEllipse}
+      alt="ellipse"
+      width="317"
+      height="505"
+      class="ellipse"
+      bind:this={ellipseElement}
+    />
     <img
       src={introShapeTriangleLarge}
       alt="triangle large"
       width="461"
       height="399"
       class="triangle-large"
+      bind:this={triangleLargeElement}
     />
     <img
       src={introShapeTriangleSmall}
@@ -85,6 +304,7 @@
       width="100"
       height="86"
       class="triangle-small"
+      bind:this={triangleSmallElement}
     />
   </div>
 </section>
@@ -135,14 +355,14 @@
   }
 
   .intro-cards {
-    margin-top: 60px;
+    padding-top: 60px;
 
     @screen tb {
-      margin-top: 100px;
+      padding-top: 100px;
     }
 
     @screen pc {
-      margin-top: 136px;
+      padding-top: 136px;
     }
   }
 
@@ -168,13 +388,11 @@
     &.intro-card--svelte {
       --bg-from: #e1befc;
       --bg-to: #d49cff;
-      --transition-delay: 200ms;
     }
 
     &.intro-card--vietnam {
       --bg-from: #d0e957;
       --bg-to: #bcd155;
-      --transition-delay: 400ms;
 
       margin-top: 20px;
 
@@ -186,7 +404,6 @@
     &.intro-card--sveltevietnam {
       --bg-from: #fea74f;
       --bg-to: #fd952b;
-      --transition-delay: 600ms;
 
       margin-top: 40px;
 
@@ -222,18 +439,6 @@
     }
   }
 
-  .intro-title,
-  .intro-card {
-    transform: translateY(80px);
-
-    opacity: 0;
-
-    transition-delay: var(--transition-delay, 0ms);
-    transition-timing-function: cubic-bezier(0.45, 0.35, 0.52, 1.15);
-    transition-duration: 500ms;
-    transition-property: transform, opacity;
-  }
-
   .intro-backdrop {
     position: absolute;
     z-index: -1;
@@ -244,12 +449,6 @@
     max-width: 100%;
 
     & img {
-      /* initial transition */
-      --initial-transition-duration: 1.4s;
-      --initial-transition-delay: 0s;
-      --initial-translate-x: 0;
-      --initial-translate-y: 0;
-
       /* alternating pulsating animation */
       --animation-delay: calc(var(--initial-transition-delay) + var(--initial-transition-duration));
       --rotate-x: 0deg;
@@ -273,13 +472,6 @@
         rotateX(0) rotateY(0) rotateZ(var(--rotate-z-from)) scale(1);
 
       height: auto;
-
-      opacity: 0;
-
-      transition-delay: var(--initial-transition-delay);
-      transition-timing-function: ease-out, cubic-bezier(0.41, 0.78, 0.43, 0.96);
-      transition-duration: var(--initial-transition-duration);
-      transition-property: transform, opacity;
     }
 
     & .star {
@@ -384,34 +576,6 @@
         --top: 155px;
         --left: calc(50% + 90px);
       }
-    }
-  }
-
-  :global(html.splashed .intro.intersected) {
-    & .intro-title,
-    & .intro-card {
-      transform: translateY(0);
-      opacity: 1;
-    }
-
-    & .intro-backdrop img {
-      --initial-translate-x: 0;
-      --initial-translate-y: 0;
-
-      opacity: 1;
-      animation: shape 3s var(--animation-delay) ease-in-out alternate infinite;
-    }
-  }
-
-  @keyframes shape {
-    0% {
-      transform: translateX(0) translateY(0) rotateX(0) rotateY(0) rotateZ(var(--rotate-z-from))
-        scale(1);
-    }
-
-    100% {
-      transform: translateX(0) translateY(0) rotateX(var(--rotate-x)) rotateY(var(--rotate-y))
-        rotateZ(var(--rotate-z-to)) scale(var(--scale));
     }
   }
 </style>
