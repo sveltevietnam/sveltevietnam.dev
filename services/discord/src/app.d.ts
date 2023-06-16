9e1f3577eecc8f60de00c672979b139f74d0a203
Quang Phan <phan.quang@4digit.jp>
2023-06-16 22:58:58 +0700

    chore(@services/discord): type checking with jsconfig. Improve env loading with zod & custom fastify plugin

--- services/discord/src/app.d.ts
+++ services/discord/src/app.d.ts
@@ -1,10 +1,15 @@
+import * as fastify from 'fastify';
+import type z from 'zod';
+
+import type { schema } from './env';
+
+type EnvSchema = z.infer<typeof schema>;
+
 declare module 'fastify' {
   interface FastifyInstance {
-    config: {
-      DISCORD_TOKEN: string;
-      DISCORD_APPLICATION_ID: string;
-      DISCORD_PUBLIC_KEY: string;
-      DISCORD_SVELTEVIETNAM_GUILD_ID: string;
-    };
+    env: EnvSchema;
+  }
+  interface FastifyRequest extends FastifyRequest {
+    env: EnvSchema;
   }
 }
