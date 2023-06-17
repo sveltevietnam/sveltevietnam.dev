87c3c2fc82f2c749768df7c0bcf76de63d82b845
Quang Phan <vnphanquang@gmail.com>
2023-06-17 21:47:45 +0700

    refactor(@services/discord): encapsulate discord logics into own file

--- services/discord/src/server.js
+++ services/discord/src/server.js
@@ -1,66 +1,35 @@
+'use strict';
+
 import FastifyHelmet from '@fastify/helmet';
 import FastifyWebsocket from '@fastify/websocket';
-import { Client, GatewayIntentBits, Events } from 'discord.js';
 import Fastify from 'fastify';
-import { v4 as uuidv4 } from 'uuid';
 
+import { FastifyDiscord } from './discord.js';
 import { FastifyEnv } from './env.js';
 
 const fastify = Fastify({
   logger: true,
 });
 
-await fastify.register(FastifyEnv).register(FastifyHelmet).register(FastifyWebsocket);
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
-});
-/**
- * @type {Record<string, import('@fastify/websocket').SocketStream>}
- */
-const connections = {};
-
-client.login(fastify.env.DISCORD_TOKEN);
-client.on(Events.ClientReady, () => {
-  console.log('ready');
-});
-client.on(Events.MessageCreate, (message) => {
-  const { author, guildId } = message;
-  if (!author.bot) return;
-  if (guildId !== fastify.env.DISCORD_SVELTEVIETNAM_GUILD_ID) return;
-  for (const connection of Object.values(connections)) {
-    connection.socket.send(
-      JSON.stringify({
-        type: 'message',
-        data: {
-          id: message.id,
-          content: message.content,
-        },
-      }),
-    );
-  }
-});
+await fastify
+  .register(FastifyEnv)
+  .register(FastifyDiscord)
+  .register(FastifyWebsocket)
+  .register(FastifyHelmet);
 
 fastify.get('/', async (request, reply) => {
   reply.redirect(301, fastify.env.DISCORD_SVELTEVIETNAM_INVITE_URL);
 });
-fastify.get('/websocket', { websocket: true }, (connection) => {
-  const id = uuidv4();
-  connections[id] = connection;
-  connection.socket.on('close', () => {
-    delete connections[id];
-  });
+
+fastify.get('/websocket', { websocket: true }, (connection, request) => {
+  request.discord.connect(connection);
 });
 
 /**
  * Run the server!
  */
 try {
+  await fastify.ready();
   await fastify.listen({ port: 5006 });
 } catch (err) {
   fastify.log.error(err);
