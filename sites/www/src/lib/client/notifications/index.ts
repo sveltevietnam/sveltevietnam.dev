bb6e91b5f82fa5d8813f05fab2bb9522dfc65bf2
Quang Phan <vnphanquang@gmail.com>
2023-08-08 20:44:04 +0700

    refactor(www): extract a base Notification component, extend from there

--- sites/www/src/lib/client/notifications/index.ts
+++ sites/www/src/lib/client/notifications/index.ts
@@ -1,19 +1,40 @@
 import { store } from '@svelte-put/noti';
 
-import Notification from './Notification.svelte';
+import TextOnlyNotification from './TextOnlyNotification.svelte';
 
 export const notiStore = store({
   timeout: 5000,
 })
-  .variant('info', Notification)
-  .variant('success', Notification)
-  .variant('warning', Notification)
-  .variant('error', Notification)
+  .variant('text-only', TextOnlyNotification)
   .build();
 
 export const noti = {
-  info: (content: string) => notiStore.push('info', { props: { content } }),
-  success: (content: string) => notiStore.push('success', { props: { content } }),
-  warning: (content: string) => notiStore.push('warning', { props: { content } }),
-  error: (content: string) => notiStore.push('error', { props: { content } }),
+  info: (content: string) =>
+    notiStore.push('text-only', {
+      props: {
+        content,
+        intent: 'info',
+      },
+    }),
+  success: (content: string) =>
+    notiStore.push('text-only', {
+      props: {
+        content,
+        intent: 'success',
+      },
+    }),
+  warning: (content: string) =>
+    notiStore.push('text-only', {
+      props: {
+        content,
+        intent: 'warning',
+      },
+    }),
+  error: (content: string) =>
+    notiStore.push('text-only', {
+      props: {
+        content,
+        intent: 'error',
+      },
+    }),
 };
