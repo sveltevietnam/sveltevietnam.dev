b7a9dbae64cc61a8a249cc5cbb2d29c73b47b54d
Quang Phan <phan.quang@4digit.jp>
2023-08-08 15:06:48 +0700

    feat(sites): setup @tailwind/typography for blog post

--- sites/www/tailwind.config.cjs
+++ sites/www/tailwind.config.cjs
@@ -2,12 +2,27 @@ const sveltevietnam = require('./src/lib/client/styles/postcss/tailwind.cjs');
 
 /** @type {import("tailwindcss").Config } */
 const config = {
+  theme: {
+    extend: {
+      typography: ({ theme }) => {
+        return {
+          DEFAULT: {
+            css: {
+              '--tw-prose-body': theme('colors.design.fg.1'),
+              '--tw-prose-invert-body': theme('colors.design.fg.1'),
+            },
+          },
+        };
+      },
+    },
+  },
   darkMode: '',
   content: ['./src/**/*.{html,js,svelte,ts,md}', 'svelte.config.js'],
   plugins: [
     require('@tailwindash/triangle'),
     sveltevietnam,
     require('postcss-color-scheme/lib/tailwind'),
+    require('@tailwindcss/typography'),
   ],
 };
 
