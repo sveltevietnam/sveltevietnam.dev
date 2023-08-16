711a62fb26258802269f5e0c0fe1a0eb9f000968
Quang Phan <vnphanquang@gmail.com>
2023-08-16 18:52:43 +0700

    feat(www): initial implementation for Breadcrumbs component

---
+++ sites/www/src/lib/client/components/Breadcrumbs/Breadcrumbs.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Breadcrumb } from '$shared/services/navigation';
+
+  export let breadcrumbs: Breadcrumb[];
+  let cls = '';
+  export { cls as class };
+</script>
+
+<nav aria-label="breadcrumb" class="tp-body2 {cls}">
+  <ol class="flex items-center gap-3">
+    {#each breadcrumbs as { label, href }, i}
+      {@const last = i === breadcrumbs.length - 1}
+      <li {...last && { 'aria-current': 'page' }} class="contents">
+        {#if href}
+          <a {href} class="c-link-neutral">{label}</a>
+        {:else}
+          <span class="text-design-neutral-2">{label}</span>
+        {/if}
+        {#if !last}
+          <svg inline-src="icon/caret" width="14" height="14" />
+        {/if}
+      </li>
+    {/each}
+  </ol>
+</nav>
