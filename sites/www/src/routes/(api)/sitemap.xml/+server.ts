ea4f86c1010e75501e4fcb66b61faa60e71ed410
Quang Phan <phan.quang@4digit.jp>
2023-08-31 17:43:06 +0700

    chore(www): setup sitemap.xml for all current pages

--- sites/www/src/routes/(api)/sitemap.xml/+server.ts
+++ sites/www/src/routes/(api)/sitemap.xml/+server.ts
@@ -1,5 +1,17 @@
 import Mustache from 'mustache';
 
+import { LANGUAGES } from '$shared/services/i18n';
+import {
+  BLOG_PATH,
+  CODE_OF_CONDUCT_PATH,
+  DESIGN_PATH,
+  DESIGN_TYPOGRAPHY_PATH,
+  EVENTS_PATH,
+  IMPACT_PATH,
+  JOBS_PATH,
+  PEOPLE_PATH,
+  SPONSOR_PATH,
+} from '$shared/services/navigation';
 import { toW3CDate } from '$shared/utils/datetime';
 
 import type { RequestHandler } from './$types';
@@ -11,29 +23,74 @@ type SiteMapUrl = {
   lastmod?: string;
   changefreq?: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
   priority?: number;
+  alternates?: Array<{
+    hreflang: string;
+    href: string;
+  }>;
 };
 
 export const GET: RequestHandler = ({ url }) => {
   const urls: SiteMapUrl[] = [
-    // {
-    //   loc: `${url.origin}/${...}`,
-    //   changefreq: 'monthly',
-    //   priority: 0.9,
-    // },
-    // ...Object.values(packages).map(
-    //   (pkg) =>
-    //     ({
-    //       loc: `${url.origin}/${pkg.path}`,
-    //       changefreq: 'monthly',
-    //       priority: 0.8,
-    //     } satisfies SiteMapUrl),
-    // ),
-    {
-      loc: url.origin,
+    ...makeUrl({
+      loc: `${url.origin}`,
       lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
       changefreq: 'monthly',
+      priority: 0.5,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${BLOG_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'monthly',
+      priority: 0.9,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${EVENTS_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'monthly',
+      priority: 0.8,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${JOBS_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'monthly',
+      priority: 0.7,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${IMPACT_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'monthly',
+      priority: 0.6,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${PEOPLE_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'yearly',
+      priority: 0.5,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${SPONSOR_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'yearly',
+      priority: 0.4,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${CODE_OF_CONDUCT_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'yearly',
+      priority: 0.3,
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${DESIGN_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'yearly',
       priority: 0.2,
-    },
+    }),
+    ...makeUrl({
+      loc: `${url.origin}${DESIGN_TYPOGRAPHY_PATH}`,
+      lastmod: toW3CDate(parseInt(__BUILD_TIMESTAMP__, 10)),
+      changefreq: 'yearly',
+      priority: 0.2,
+    }),
   ];
   const xml = Mustache.render(template, { urls });
   const headers = {
@@ -42,3 +99,16 @@ export const GET: RequestHandler = ({ url }) => {
   };
   return new Response(xml, { headers });
 };
+
+function makeUrl(url: SiteMapUrl): SiteMapUrl[] {
+  return LANGUAGES.map((locLang) => ({
+    ...url,
+    loc: `${url.loc}/${locLang}`,
+    alternates:
+      url.alternates ??
+      LANGUAGES.map((lang) => ({
+        href: `${url.loc}/${lang}`,
+        hreflang: lang,
+      })),
+  }));
+}
