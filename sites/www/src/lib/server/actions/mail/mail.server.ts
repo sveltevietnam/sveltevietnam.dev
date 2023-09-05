ffc701ead5bf69adc57e30c94bcf79b3685b09c0
Quang Phan <phan.quang@4digit.jp>
2023-09-05 13:16:11 +0700

    chore: test mailchannels send API

--- sites/www/src/lib/server/actions/mail/mail.server.ts
+++ sites/www/src/lib/server/actions/mail/mail.server.ts
@@ -4,6 +4,7 @@ import { message, setError, superValidate } from 'sveltekit-superforms/server';
 import { z } from 'zod';
 
 import type { MailMessage } from '$client/components/MailRegistrationForm';
+import { DKIM_DOMAIN, DKIM_PRIVATE_KEY, DKIM_SELECTOR } from '$env/static/private';
 import { validateToken } from '$server/services/turnstile';
 
 import { translations } from './translation';
@@ -72,6 +73,46 @@ export async function mail<E extends RequestEvent>(event: E, domain: 'job' | 'ev
       )
       .bind(form.data.email, form.data.name)
       .run();
+
+    // notify user
+    const body = {
+      personalizations: [
+        {
+          to: [
+            {
+              email: form.data.email,
+              name: form.data.name,
+            },
+          ],
+          dkim_domain: DKIM_DOMAIN,
+          dkim_selector: DKIM_SELECTOR,
+          dkim_private_key: DKIM_PRIVATE_KEY,
+        },
+      ],
+      from: {
+        email: 'no-reply@sveltevietnam.dev',
+        name: 'Svelte Vietnam',
+      },
+      // TODO: i18n
+      subject: 'Svelte Vietnam: Registration Confirmation',
+      content: [
+        {
+          type: 'text/plain',
+          value: `Thank you for registering for ${domain}!`,
+        },
+      ],
+    };
+    const resp = await fetch('https://api.mailchannels.net/tx/v1/send', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const text = await resp.text();
+
+    console.log(`Turbo ~ file: mail.server.ts:109 ~ text:`, resp.status, resp.statusText, text);
 
     return message(form, {
       type: 'success',
