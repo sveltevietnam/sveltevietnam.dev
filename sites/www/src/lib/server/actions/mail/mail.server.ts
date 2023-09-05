f714c102162f9a78a330bc58593cf04846619a67
Quang Phan <phan.quang@4digit.jp>
2023-09-05 15:59:40 +0700

    chore(www): turn off registration email notification (MailChannels) for now

--- sites/www/src/lib/server/actions/mail/mail.server.ts
+++ sites/www/src/lib/server/actions/mail/mail.server.ts
@@ -75,44 +75,43 @@ export async function mail<E extends RequestEvent>(event: E, domain: 'job' | 'ev
       .run();
 
     // notify user
-    const body = {
-      personalizations: [
-        {
-          to: [
-            {
-              email: form.data.email,
-              name: form.data.name,
-            },
-          ],
-          dkim_domain: DKIM_DOMAIN,
-          dkim_selector: DKIM_SELECTOR,
-          dkim_private_key: DKIM_PRIVATE_KEY,
-        },
-      ],
-      from: {
-        email: 'no-reply@sveltevietnam.dev',
-        name: 'Svelte Vietnam',
-      },
-      // TODO: i18n
-      subject: 'Svelte Vietnam: Registration Confirmation',
-      content: [
-        {
-          type: 'text/plain',
-          value: `Thank you for registering for ${domain}!`,
-        },
-      ],
-    };
-    const resp = await fetch('https://api.mailchannels.net/tx/v1/send', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-
-    const text = await resp.text();
-
-    console.log(`Turbo ~ file: mail.server.ts:109 ~ text:`, resp.status, resp.statusText, text);
+    // const body = {
+    //   personalizations: [
+    //     {
+    //       to: [
+    //         {
+    //           email: form.data.email,
+    //           name: form.data.name,
+    //         },
+    //       ],
+    //       dkim_domain: DKIM_DOMAIN,
+    //       dkim_selector: DKIM_SELECTOR,
+    //       dkim_private_key: DKIM_PRIVATE_KEY,
+    //     },
+    //   ],
+    //   from: {
+    //     email: 'no-reply@sveltevietnam.dev',
+    //     name: 'Svelte Vietnam',
+    //   },
+    //   // TODO: i18n
+    //   subject: 'Svelte Vietnam: Registration Confirmation',
+    //   content: [
+    //     {
+    //       type: 'text/plain',
+    //       value: `Thank you for registering for ${domain}!`,
+    //     },
+    //   ],
+    // };
+    // const resp = await fetch('https://api.mailchannels.net/tx/v1/send', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify(body),
+    // });
+    // TODO: add error capturing
+    // const text = await resp.text();
+    // console.log(`Turbo ~ file: mail.server.ts:109 ~ text:`, resp.status, resp.statusText, text);
 
     return message(form, {
       type: 'success',
