46943a7ea4ccb76bc43175e45c72cd4666947426
Quang Phan <vnphanquang@gmail.com>
2023-09-11 18:45:25 +0700

    style(www): apply intersect intro animation to /roadmap page

--- sites/www/src/routes/[[lang=lang]]/roadmap/+page.svelte
+++ sites/www/src/routes/[[lang=lang]]/roadmap/+page.svelte
@@ -14,11 +14,11 @@
     <Breadcrumbs breadcrumbs={data.breadcrumbs} />
   </div>
   <section class="mt-8 tb:mt-[60px] flex items-start justify-between gap-8 upto-tb:flex-col">
-    <div>
+    <div use:intersect>
       <h1 class="tp-h1 uppercase">{t.title}</h1>
       <p class="tp-h4 mt-6 tb:mt-8">{t.subtitle}</p>
     </div>
-    <div>
+    <div use:intersect>
       <svg
         inline-src="./_page/images/key-visuals"
         class="upto-tb:w-full upto-tb:h-auto opacity-5"
@@ -29,83 +29,93 @@
   <div class="space-y-[60px] tb:space-y-[120px] py-[60px] tb:pb-[200px] tb:pt-[120px]">
     <section class="relative">
       <div class="space-y-6">
-        <h2 class="tp-h2 font-medium">{t.common.milestone}: {t.milestones.aulac.title}</h2>
-        <p>
+        <h2 class="tp-h2 font-medium" use:intersect>
+          {t.common.milestone}: {t.milestones.aulac.title}
+        </h2>
+        <p use:intersect>
           <strong class="font-medium">{t.common.objective}</strong>: {t.milestones.aulac.objective}
         </p>
         <div>
-          <p class="font-medium">{t.common.tasks}:</p>
+          <p class="font-medium" use:intersect>{t.common.tasks}:</p>
           <ul class="space-y-3 ml-3 mt-3">
             {#each t.milestones.aulac.primaryTasks as task}
-              <li>
+              <li use:intersect>
                 {@html task}
               </li>
             {/each}
           </ul>
         </div>
       </div>
-      <svg
-        inline-src="./_page/images/au-lac"
-        class="absolute top-0 right-0 -z-px opacity-[2%] upto-tb:w-full upto-tb:h-auto upto-tb:top-[100px]"
-      />
+      <div use:intersect class="absolute top-0 right-0 -z-px">
+        <svg
+          inline-src="./_page/images/au-lac"
+          class="opacity-[2%] upto-tb:w-full upto-tb:h-auto upto-tb:top-[100px]"
+        />
+      </div>
     </section>
 
     <section class="relative">
       <div class="space-y-6">
-        <h2 class="tp-h2 font-medium">{t.common.milestone}: {t.milestones.tu.title}</h2>
-        <p>
+        <h2 class="tp-h2 font-medium" use:intersect>
+          {t.common.milestone}: {t.milestones.tu.title}
+        </h2>
+        <p use:intersect>
           <strong class="font-medium">{t.common.objective}</strong>: {t.milestones.tu.objective}
         </p>
         <div>
-          <p class="font-medium">{t.common.tasks}:</p>
+          <p class="font-medium" use:intersect>{t.common.tasks}:</p>
           <ul class="space-y-3 ml-3 mt-3">
             {#each t.milestones.tu.primaryTasks as task}
-              <li>
+              <li use:intersect>
                 {@html task}
               </li>
             {/each}
           </ul>
         </div>
         <div>
-          <p><strong class="font-medium">{t.milestones.tu.secondaryTasks.title}:</strong></p>
+          <p use:intersect>
+            <strong class="font-medium">{t.milestones.tu.secondaryTasks.title}:</strong>
+          </p>
           <ul class="space-y-3 ml-3 mt-3">
             {#each t.milestones.tu.secondaryTasks.items as task}
-              <li>
+              <li use:intersect>
                 {@html task}
               </li>
             {/each}
           </ul>
         </div>
       </div>
-      <div
-        class="absolute bottom-0 right-0 -z-px flex items-center gap-10 upto-tb:flex-col upto-tb:max-h-full opacity-[2%]"
-      >
-        <p class="sr-only">Chữ Nôm</p>
-        <svg inline-src="./_page/images/tu_chu.svg" />
-        <svg inline-src="./_page/images/tu_nom.svg" />
+      <div class="absolute bottom-0 right-0 -z-px" use:intersect>
+        <div class="flex items-center gap-10 upto-tb:flex-col upto-tb:max-h-full opacity-[2%]">
+          <p class="sr-only">Chữ Nôm</p>
+          <svg inline-src="./_page/images/tu_chu.svg" />
+          <svg inline-src="./_page/images/tu_nom.svg" />
+        </div>
       </div>
     </section>
 
     <section class="relative">
       <div class="space-y-6">
-        <h2 class="tp-h2 font-medium">{t.common.milestone}: {t.milestones.dong.title}</h2>
-        <p>
+        <h2 class="tp-h2 font-medium" use:intersect>
+          {t.common.milestone}: {t.milestones.dong.title}
+        </h2>
+        <p use:intersect>
           <strong class="font-medium">{t.common.objective}</strong>: {t.milestones.dong.objective}
         </p>
         <div>
-          <p class="font-medium">{t.common.tasks}:</p>
+          <p class="font-medium" use:intersect>{t.common.tasks}:</p>
           <ul class="space-y-3 ml-3 mt-3">
             {#each t.milestones.dong.primaryTasks as task}
-              <li>
+              <li use:intersect>
                 {@html task}
               </li>
             {/each}
           </ul>
         </div>
       </div>
-      <div class="absolute bottom-0 right-0 -z-px max-h-full opacity-[2%]">
+      <div class="absolute bottom-0 right-0 -z-px" use:intersect>
         <p class="sr-only">Đồng Thiên Phúc trấn bảo</p>
-        <svg inline-src="./_page/images/dong" />
+        <svg inline-src="./_page/images/dong" class="opacity-[2%] max-h-full" />
       </div>
     </section>
   </div>
