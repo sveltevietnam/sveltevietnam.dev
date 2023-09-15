51f5daec9d14ee05b6272ea4f659c8621f3c9566
Quang Phan <phan.quang@4digit.jp>
2023-09-15 22:28:27 +0700

    feat(mailer): /subscribe POST endpoint

---
+++ services/mailer/src/routes/(protected)/subscribe/+server.ts
@@ -0,0 +1,53 @@
+import { json } from '@sveltejs/kit';
+import { string as zString, object as zObject, enum as zEnum } from 'zod';
+
+import {
+  SUBSCRIPTION_DOMAINS,
+  getSubscriptionByEmail,
+  upsertSubscription,
+} from '$server/daos/subscriptions.dao';
+import { createErrorResponse } from '$server/errors';
+
+import type { RequestHandler } from './$types';
+
+const schema = zObject({
+  name: zString().min(1),
+  email: zString().min(1).email(),
+  domain: zEnum(SUBSCRIPTION_DOMAINS),
+});
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+  const parsed = schema.safeParse(await request.json());
+  if (!parsed.success) {
+    return createErrorResponse(
+      'SUBSCRIPTION_INVALID_INPUT',
+      parsed.error.errors.map((e) => e.message),
+    );
+  }
+
+  const { d1 } = locals;
+  const { email, domain, name } = parsed.data;
+
+  try {
+    // pass through if email has already been registered for this domain
+    const subscription = await getSubscriptionByEmail(d1, email);
+    if (subscription?.[domain]) {
+      return createErrorResponse('SUBSCRIPTION_EXISTS');
+    }
+
+    // otherwise upsert subscription
+    await upsertSubscription(d1, domain, { name, email });
+  } catch (e) {
+    // add error capturing
+    return createErrorResponse('SUBSCRIPTION_UNKNOWN_ERROR');
+  }
+
+  return json(
+    {
+      success: true,
+    },
+    {
+      status: 201,
+    },
+  );
+};
