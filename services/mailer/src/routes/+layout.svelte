279ac5aa5db84fedc4b3a9b7b516a53f7e6be264
Quang Phan <vnphanquang@gmail.com>
2023-09-17 13:28:51 +0700

    chore(mailer): github deployment workflow

--- services/mailer/src/routes/+layout.svelte
+++ services/mailer/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import { PUBLIC_MODE } from '$env/static/public';
+
   import '../lib/client/styles/app.css';
 </script>
+
+<svelte:head>
+  <title>Mailer Service | Svelte Vietnam</title>
+  <meta name="description" content="Internal mailer service of Svelte Vietnam" />
+  <meta name="keywords" content="mail, email, subscription, internal, service" />
+  <meta name="mode" content={PUBLIC_MODE} />
+</svelte:head>
 
 <slot />
