9b9ece173c5aced2c35b93ffe3c97ca80a82316f
Quang Phan <phan.quang@4digit.jp>
2023-09-18 16:37:46 +0700

    refactor(mailer): define id manually during insertion (mails table)

--- services/mailer/src/lib/server/daos/mails.dao.ts
+++ services/mailer/src/lib/server/daos/mails.dao.ts
@@ -3,7 +3,7 @@ import type { D1Database } from '@cloudflare/workers-types';
 const TABLE_NAME = 'mails';
 
 export type Mail = {
-  id: number;
+  id: string;
   email: string;
   html: string;
   created_at: string;
@@ -13,9 +13,9 @@ export function getMailById(d1: D1Database, id: string) {
   return d1.prepare(`SELECT * FROM ${TABLE_NAME} WHERE id = ?`).bind(id).first<Mail>();
 }
 
-export function createMail(d1: D1Database, email: string, html: string) {
+export function createMail(d1: D1Database, { id, email, html, created_at }: Mail) {
   return d1
-    .prepare(`INSERT INTO ${TABLE_NAME} (email,html,created_at) VALUES(?1,?2,?3)`)
-    .bind(email, html, new Date().toISOString())
+    .prepare(`INSERT INTO ${TABLE_NAME} (id,email,html,created_at) VALUES(?1,?2,?3,?4)`)
+    .bind(id, email, html, created_at)
     .run();
 }
