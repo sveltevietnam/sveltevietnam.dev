9f02707c2e3c8e5086ecf5475229728b466f476b
Quang Phan <phan.quang@4digit.jp>
2023-09-21 17:07:03 +0700

    fix: regenerate templates

--- services/mailer/.lintstagedrc.js
+++ services/mailer/.lintstagedrc.js
@@ -3,4 +3,5 @@ import baseConfig from '../../.lintstagedrc.js';
 export default {
   ...baseConfig,
   '*.template.mjml': ['node scripts/lint-staged-mjml.js'],
+  // TODO: re-generate all template if includes/* changes
 };
