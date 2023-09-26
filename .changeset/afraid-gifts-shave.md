243b9b52838190730c9ee3ca3115d34b615e8060
Quang Phan <phan.quang@4digit.jp>
2023-09-26 12:58:55 +0700

    feature(www): ToBeAnnounced - figure variant as the default

---
+++ .changeset/afraid-gifts-shave.md
@@ -0,0 +1,5 @@
+---
+'@sites/www': patch
+---
+
+add a `figure` variant to the `ToBeAnnounced` component that uses a more complex and interseting illustration
--- sites/www/src/lib/client/components/ToBeAnnounced/ToBeAnnounced.svelte
+++ sites/www/src/lib/client/components/ToBeAnnounced/ToBeAnnounced.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
+  import figure from './tba-figure.webp';
+  export let variant: 'text' | 'figure' = 'figure';
   let cls = '';
   export { cls as class };
 </script>
 
-<div class="w-full border rounded-[16px] border-design-fg-1 pt-20 pb-14 {cls}">
-  <svg inline-src="tba" width="323" height="94" class="mx-auto max-w-full" />
-  <div class="mt-10">
-    <slot />
+{#if variant === 'text'}
+  <div class="w-full border rounded-[16px] border-design-fg-1 pt-20 pb-14 {cls}">
+    <svg inline-src="tba" width="323" height="94" class="mx-auto max-w-full" />
+    <div class="mt-10">
+      <slot />
+    </div>
   </div>
-</div>
+{:else if variant === 'figure'}
+  <div class={cls}>
+    <img src={figure} alt="to be announced" width="357" height="357" class="m-auto max-w-full" />
+    <div class="mt-6">
+      <slot />
+    </div>
+  </div>
+{/if}
