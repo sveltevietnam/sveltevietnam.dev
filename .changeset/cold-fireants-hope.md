bd83de7a8c1fccf678804d8044e246310b6de4f9
Quang Phan <vnphanquang@gmail.com>
2023-10-12 21:41:37 +0700

    feat(www): tooltip for language indicator - blog post layout

---
+++ .changeset/cold-fireants-hope.md
@@ -0,0 +1,5 @@
+---
+'@sites/www': minor
+---
+
+Add a text tooltip with description for the language indicator in blog post layout
--- sites/www/src/lib/client/styles/components/index.cjs
+++ sites/www/src/lib/client/styles/components/index.cjs
@@ -2,6 +2,7 @@ require('../postcss/css-node-loader.cjs');
 
 const components = {
   // HYGEN INJECTION MARKER
+  ...require('./tooltip.css'),
   ...require('./tag.css'),
   ...require('./loader.css'),
   ...require('./action-card.css'),
