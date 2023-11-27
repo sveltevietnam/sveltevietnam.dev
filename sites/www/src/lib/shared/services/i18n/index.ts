a135308884a5b039a5713fd0e14f8278977905c4
Quang Phan <vnphanquang@gmail.com>
2023-11-27 14:54:34 +0700

    refactor(www): i18n resolveLangVar cover case where T is Array

--- sites/www/src/lib/shared/services/i18n/index.ts
+++ sites/www/src/lib/shared/services/i18n/index.ts
@@ -3,6 +3,6 @@ export type Language = (typeof LANGUAGES)[number];
 export type LangVar<T = string> = string | Record<Language, T>;
 
 export function resolveLangVar<T>(lang: Language, v?: LangVar<T>): T | undefined {
-	if (typeof v === 'object') return v?.[lang] ?? undefined;
+	if (typeof v === 'object' && !Array.isArray(v)) return v?.[lang] ?? undefined;
 	return v as T;
 }
