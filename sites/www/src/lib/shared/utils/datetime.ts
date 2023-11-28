c9444c424c630ab7439bfc49211f5cfb70cdf245
Quang Phan <vnphanquang@gmail.com>
2023-11-28 22:00:29 +0700

    feat(www): consistent datetime formatter for events

--- sites/www/src/lib/shared/utils/datetime.ts
+++ sites/www/src/lib/shared/utils/datetime.ts
@@ -13,10 +13,15 @@ export function toW3CDate(date: Date | number): string {
 		.padStart(2, '0')}`;
 }
 
+function isValidDate(str: Date | number | string) {
+	return !isNaN(new Date(str).getTime());
+}
+
 /**
  * format date to YYYY.MM.DD
  */
 export function formatDate(date: Date | number | string): string {
+	if (!isValidDate(date)) return date.toString();
 	date = new Date(date);
 	return `${date.getFullYear()}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date
 		.getDate()
@@ -24,7 +29,26 @@ export function formatDate(date: Date | number | string): string {
 		.padStart(2, '0')}`;
 }
 
+/**
+ * format time to HH:MM
+ */
+export function formatTime(date: Date | number | string): string {
+	if (!isValidDate(date)) return date.toString();
+	date = new Date(date);
+	return `${date.getHours().toString().padStart(2, '0')}:${date
+		.getMinutes()
+		.toString()
+		.padStart(2, '0')}`;
+}
+
+export function formatDateAndTime(date: Date | number | string): string {
+	if (!isValidDate(date)) return date.toString();
+	date = new Date(date);
+	return `${formatDate(date)} ${formatTime(date)}`;
+}
+
 export function formateDateForBlog(lang: Language, date: Date | number | string): string {
+	if (!isValidDate(date)) return date.toString();
 	const str = new Date(date).toLocaleDateString(lang, {
 		month: 'long',
 		year: 'numeric',
