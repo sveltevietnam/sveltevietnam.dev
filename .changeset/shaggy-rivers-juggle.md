a8456af694006dba700e8f8dd647ea2e3575cca2
Quang Phan <vnphanquang@gmail.com>
2023-12-02 16:39:37 +0700

    enhance(www): progressively enhance ColorSchemeMenu

---
+++ .changeset/shaggy-rivers-juggle.md
@@ -0,0 +1,5 @@
+---
+'@sites/www': minor
+---
+
+progressive enhancement support for `ColorSchemeMenu` (no JS dropdown, fallback form submission with query param for no JS users) (#185)
--- sites/www/src/lib/client/components/Header/components/ColorSchemeMenu.svelte
+++ sites/www/src/lib/client/components/Header/components/ColorSchemeMenu.svelte
@@ -1,155 +1,82 @@
 <script lang="ts">
-	import { clickoutside } from '@svelte-put/clickoutside';
-	import { slide, fade } from 'svelte/transition';
-
 	import { getColorSchemeContext } from '$client/contexts/color-scheme';
 	import { getLangContext } from '$client/contexts/lang';
+	import { COLOR_SCHEMES } from '$shared/constants';
 	import { translations } from '$shared/services/i18n/translations/color-scheme';
 
 	import ColorSchemeIcon from './ColorSchemeIcon.svelte';
 
-	const langStore = getLangContext();
-	$: lang = $langStore;
-
 	let cls = '';
 	export { cls as class };
 
-	const SCHEMES = {
-		light: {
-			scheme: 'light',
-		},
-		dark: {
-			scheme: 'dark',
-		},
-		system: {
-			scheme: 'system',
-		},
-	} as const;
-	let themeMenuOpen = false;
-	function toggleThemeMenu() {
-		themeMenuOpen = !themeMenuOpen;
-	}
+	const langStore = getLangContext();
+	$: lang = $langStore;
 	$: t = translations[lang];
 
 	const colorSchemeStore = getColorSchemeContext();
 </script>
 
-<div class="color-scheme-menu {cls}" data-expanded={themeMenuOpen}>
-	{#key $colorSchemeStore}
-		<button
-			class="color-scheme-menu__toggler"
-			on:click|stopPropagation={toggleThemeMenu}
-			in:fade|local={{ duration: 150 }}
-			aria-label="Color scheme menu"
-		>
-			<ColorSchemeIcon scheme={SCHEMES[$colorSchemeStore].scheme} width={24} height={24} />
-		</button>
-	{/key}
-	{#key themeMenuOpen}
-		<div
-			class="color-scheme-menu__dropdown"
-			class:hidden={!themeMenuOpen}
-			transition:slide|local={{ duration: 150 }}
-			use:clickoutside={{ enabled: themeMenuOpen }}
-			on:clickoutside={toggleThemeMenu}
-		>
-			<div>
-				<svg
-					width="12"
-					height="11"
-					viewBox="0 0 12 11"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M8.16506 1.75L10.7631 6.25C11.7254 7.91667 10.5226 10 8.59808 10H3.40192C1.47742 10 0.274609 7.91667 1.23686 6.25L3.83494 1.75C4.79719 0.0833309 7.20281 0.0833325 8.16506 1.75Z"
-						fill="white"
-						stroke="currentcolor"
-					/>
-					<rect y="7" width="12" height="4" fill="white" />
-				</svg>
-				<ul class="tp-body2">
-					{#each Object.values(SCHEMES) as s}
-						<li>
-							<button
-								on:click={() => colorSchemeStore.change(s.scheme)}
-								class="color-scheme-menu__option"
-								class:text-link={$colorSchemeStore === s.scheme}
+<div class="color-scheme-menu csm relative {cls}">
+	<label
+		for="csm-toggler"
+		class="grid h-8 cursor-pointer place-items-center rounded-2xl border border-current px-3 hover:text-link"
+	>
+		<ColorSchemeIcon scheme={$colorSchemeStore} width={24} height={24} />
+	</label>
+	<input type="checkbox" checked={false} id="csm-toggler" hidden />
+	<div class="csm-dropdown">
+		<div class="overflow-hidden">
+			<svg inline-src="./arrow.svg" class="csm-dropdown-arrow" />
+			<ul class="relative rounded-lg border border-current bg-bg">
+				{#each COLOR_SCHEMES as scheme}
+					<li>
+						<form method="GET" on:submit|preventDefault={() => colorSchemeStore.change(scheme)}>
+							<label
+								class="w-100% flex cursor-pointer items-center p-2.5 hover:text-link"
+								class:text-link={scheme === $colorSchemeStore}
 							>
-								<ColorSchemeIcon scheme={s.scheme} />
-								<span class="whitespace-nowrap">{t[s.scheme]}</span>
-							</button>
-						</li>
-					{/each}
-				</ul>
-			</div>
+								<input type="submit" value={scheme} name="color-scheme" hidden />
+								<ColorSchemeIcon {scheme} />
+								<span class="tp-body2 ml-2">{t[scheme]}</span>
+							</label>
+						</form>
+					</li>
+				{/each}
+			</ul>
 		</div>
-	{/key}
+	</div>
 </div>
 
 <style lang="postcss">
-	.color-scheme-menu {
-		position: relative;
-		display: grid;
-		place-items: center;
-	}
-
-	.color-scheme-menu__toggler {
-		height: 32px;
-		padding: 0 12px;
-		border: 1px solid currentcolor;
-		border-radius: 16px;
-
-		&:hover {
-			color: theme('colors.link.DEFAULT');
-		}
-	}
-
-	.color-scheme-menu__option {
-		display: flex;
-		align-items: center;
-		width: 100%;
-		padding: 10px;
-
-		&:hover {
-			color: theme('colors.link.DEFAULT');
-		}
-
-		& span {
-			margin-left: 8px;
-		}
-	}
-
-	.color-scheme-menu__dropdown {
+	.csm-dropdown {
 		position: absolute;
 		z-index: theme('zIndex.popup');
-		top: calc(100% + 10px);
+		top: calc(100% + 5px);
 		left: 50%;
 		transform: translate(-50%);
 
-		& div {
-			display: flex;
-			flex-direction: column;
-			align-items: center;
+		display: grid;
+		grid-template-rows: 0fr;
 
-			& svg {
-				z-index: 2;
+		width: max-content;
 
-				& rect,
-				& path {
-					fill: theme('colors.bg.DEFAULT');
-				}
-			}
+		transition: grid-template-rows 150ms ease-out;
+	}
 
-			& ul {
-				position: relative;
-				z-index: 1;
-				top: -5px;
+	#csm-toggler:checked + .csm-dropdown {
+		grid-template-rows: 1fr;
+	}
 
-				background-color: theme('colors.bg.DEFAULT');
-				border: 1px solid currentcolor;
-				border-radius: 8px;
-			}
+	svg {
+		position: relative;
+		z-index: 2;
+		top: 5px;
+		left: 50%;
+		transform: translateX(-50%);
+
+		& rect,
+		& path {
+			fill: theme('colors.bg.DEFAULT');
 		}
 	}
 </style>
