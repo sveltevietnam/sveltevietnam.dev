ca7d35ac803955938c2b885207dadc2e04700450
Quang Phan <vnphanquang@gmail.com>
2023-12-02 16:55:58 +0700

    enhance(www): ColorSchemeMenu - add back clickoutside support

--- sites/www/src/lib/client/components/Header/components/ColorSchemeMenu.svelte
+++ sites/www/src/lib/client/components/Header/components/ColorSchemeMenu.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { clickoutside } from '@svelte-put/clickoutside';
+
 	import { getColorSchemeContext } from '$client/contexts/color-scheme';
 	import { getLangContext } from '$client/contexts/lang';
 	import { COLOR_SCHEMES } from '$shared/constants';
@@ -14,23 +16,53 @@
 	$: t = translations[lang];
 
 	const colorSchemeStore = getColorSchemeContext();
+	let open = false;
+
+	function toggle(force?: boolean) {
+		open = force ?? !open;
+	}
 </script>
 
+<!-- eslint-disable svelte/valid-compile -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <div class="color-scheme-menu csm relative {cls}">
 	<label
 		for="csm-toggler"
 		class="grid h-8 cursor-pointer place-items-center rounded-2xl border border-current px-3 hover:text-link"
+		on:click|stopPropagation
 	>
 		<ColorSchemeIcon scheme={$colorSchemeStore} width={24} height={24} />
 	</label>
-	<input type="checkbox" checked={false} id="csm-toggler" hidden />
-	<div class="csm-dropdown">
+	<input type="checkbox" id="csm-toggler" hidden bind:checked={open} />
+	<div class="csm-dropdown" use:clickoutside={{ enabled: open }} on:clickoutside={() => toggle()}>
 		<div class="overflow-hidden">
-			<svg inline-src="./arrow.svg" class="csm-dropdown-arrow" />
+			<svg
+				width="12"
+				height="11"
+				viewBox="0 0 12 11"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				class="csm-dropdown-arrow"
+			>
+				<path
+					d="M8.16506 1.75L10.7631 6.25C11.7254 7.91667 10.5226 10 8.59808 10H3.40192C1.47742 10 0.274609 7.91667 1.23686 6.25L3.83494 1.75C4.79719 0.0833309 7.20281 0.0833325 8.16506 1.75Z"
+					fill="white"
+					stroke="currentcolor"
+				/>
+				<rect y="7" width="12" height="4" fill="white" />
+			</svg>
+
 			<ul class="relative rounded-lg border border-current bg-bg">
 				{#each COLOR_SCHEMES as scheme}
 					<li>
-						<form method="GET" on:submit|preventDefault={() => colorSchemeStore.change(scheme)}>
+						<form
+							method="GET"
+							on:submit|preventDefault={() => {
+								colorSchemeStore.change(scheme);
+								toggle(false);
+							}}
+						>
 							<label
 								class="w-100% flex cursor-pointer items-center p-2.5 hover:text-link"
 								class:text-link={scheme === $colorSchemeStore}
