067d6495debda161568276fa2df39ee8faa873cb
Quang Phan <vnphanquang@gmail.com>
2023-12-16 23:00:44 +0700

    chore(www): turn on listing for secrets blog post

--- sites/www/src/lib/shared/data/blog/index.ts
+++ sites/www/src/lib/shared/data/blog/index.ts
@@ -5,13 +5,13 @@ import { post as post_20231009_behind_the_screen_a_yes_code_blog_of_svelte_vietn
 import { post as post_20231012_svelte_vietnam_from_local_to_global } from '$routes/[[lang=lang]]/blog/(posts)/20231012-svelte-vietnam-from-local-to-global/_page/data';
 import { post as post_20231020_lets_write_a_simple_svelte_preprocessor } from '$routes/[[lang=lang]]/blog/(posts)/20231020-lets-write-a-simple-svelte-preprocessor/_page/data';
 import { post as post_20231110_behind_the_screen_dark_mode_with_sveltekit_tailwindcss_and_postcss } from '$routes/[[lang=lang]]/blog/(posts)/20231110-behind-the-screen-dark-mode-with-sveltekit-tailwindcss-and-postcss/_page/data';
-// import { post as post_20231204_behind_the_screen_a_few_secrets_of_sveltevietnam_dev } from '$routes/[[lang=lang]]/blog/(posts)/20231204-behind-the-screen-a-few-secrets-of-sveltevietnam-dev/_page/data';
+import { post as post_20231204_behind_the_screen_a_few_secrets_of_sveltevietnam_dev } from '$routes/[[lang=lang]]/blog/(posts)/20231204-behind-the-screen-a-few-secrets-of-sveltevietnam-dev/_page/data';
 // HYGEN MARKER - IMPORT [DO NOT REMOVE]
 import type { ExternalPost, Post } from '$shared/data/blog';
 
 export const INTERNAL_POSTS: Post[] = [
 	// HYGEN MARKER - POST [DO NOT REMOVE]
-	// post_20231204_behind_the_screen_a_few_secrets_of_sveltevietnam_dev,
+	post_20231204_behind_the_screen_a_few_secrets_of_sveltevietnam_dev,
 	post_20231110_behind_the_screen_dark_mode_with_sveltekit_tailwindcss_and_postcss,
 	post_20231012_svelte_vietnam_from_local_to_global,
 	post_20231009_behind_the_screen_a_yes_code_blog_of_svelte_vietnam,
