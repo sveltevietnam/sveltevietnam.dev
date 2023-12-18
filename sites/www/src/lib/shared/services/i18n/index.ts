72b8e3a42a77898d98b7b932ac789eda4e8758e1
Quang Phan <vnphanquang@gmail.com>
2023-12-18 09:08:53 +0700

    fix(www): resolveLangVar i18n util - handle empty string case

--- sites/www/src/lib/shared/services/i18n/index.ts
+++ sites/www/src/lib/shared/services/i18n/index.ts
@@ -4,8 +4,8 @@ export type LangVar<T = string> = string | Record<Language, T>;
 
 export function resolveLangVar<T>(lang: Language, v?: LangVar<T>): T {
 	if (typeof v === 'object' && !Array.isArray(v) && LANGUAGES.some((l) => l in v)) {
-		const r = v?.[lang];
-		if (!r) throw new Error(`Missing language variant "${lang}" in ${JSON.stringify(v)}`);
+		const r = v?.[lang] ?? null;
+		if (r === null) throw new Error(`Missing language variant "${lang}" in ${JSON.stringify(v)}`);
 		return r;
 	}
 	return v as T;
