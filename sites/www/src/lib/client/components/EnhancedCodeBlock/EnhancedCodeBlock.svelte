3da45cc8de0984d5bb86405536d7e233d6bce6a0
Quang Phan <vnphanquang@gmail.com>
2023-12-23 16:28:31 +0700

    chore: EnhancedCodeBlock - only render copy btn when JS is available

--- sites/www/src/lib/client/components/EnhancedCodeBlock/EnhancedCodeBlock.svelte
+++ sites/www/src/lib/client/components/EnhancedCodeBlock/EnhancedCodeBlock.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { copy, type TextResolverInput } from '@svelte-put/copy';
+	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
 
 	let trigger: HTMLButtonElement;
@@ -24,21 +25,28 @@
 			copied = false;
 		}, 1800);
 	}
+
+	let hydrated = false;
+	onMount(() => {
+		hydrated = true;
+	});
 </script>
 
 <div class="relative">
-	<button
-		bind:this={trigger}
-		class="absolute right-4 top-4 opacity-50 transition-opacity hover:opacity-100 disabled:cursor-pointer"
-		disabled={copied}
-	>
-		<span class="sr-only">Copy</span>
-		{#if copied}
-			<svg inline-src="lucide/copy-check" width="20" height="20" in:fly={{ y: 10 }} />
-		{:else}
-			<svg inline-src="lucide/copy" width="20" height="20" in:fly={{ y: 10 }} />
-		{/if}
-	</button>
+	{#if hydrated}
+		<button
+			bind:this={trigger}
+			class="absolute right-4 top-4 opacity-50 transition-opacity hover:opacity-100 disabled:cursor-pointer"
+			disabled={copied}
+		>
+			<span class="sr-only">Copy</span>
+			{#if copied}
+				<svg inline-src="lucide/copy-check" width="20" height="20" in:fly={{ y: 10 }} />
+			{:else}
+				<svg inline-src="lucide/copy" width="20" height="20" in:fly={{ y: 10 }} />
+			{/if}
+		</button>
+	{/if}
 	<div use:copy={{ trigger, text: copyText }} on:copied={onCopied}>
 		<slot>
 			<!-- <pre><code>...</code></pre> -->
