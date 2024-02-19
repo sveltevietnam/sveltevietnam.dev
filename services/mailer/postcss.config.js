1cfd18d593119e57e8b3d05d8da5b28e60fe83bf
Quang Phan <vnphanquang@gmail.com>
2024-02-20 06:30:43 +0700

    chore: deprecate cssnano

--- services/mailer/postcss.config.js
+++ services/mailer/postcss.config.js
@@ -1,16 +1,9 @@
 import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
 import postcssImport from 'postcss-import';
 import postcssNesting from 'postcss-nesting';
 import tailwindcss from 'tailwindcss';
 import tailwindCssNesting from 'tailwindcss/nesting/index.js';
 
 export default {
-	plugins: [
-		postcssImport,
-		tailwindCssNesting(postcssNesting),
-		tailwindcss,
-		autoprefixer,
-		...(process.env.NODE_ENV !== 'development' ? [cssnano] : []),
-	],
+	plugins: [postcssImport, tailwindCssNesting(postcssNesting), tailwindcss, autoprefixer],
 };
