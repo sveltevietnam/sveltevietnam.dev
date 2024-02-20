f0734b8dd836607b3c4555629d109662bfa5546f
Quang Phan <vnphanquang@gmail.com>
2024-02-20 10:31:09 +0700

    chore(mailer): gitignore generated email template

--- services/mailer/.lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-import baseConfig from '../../.lintstagedrc.js';
-
-export default {
-	...baseConfig,
-	'*.template.mjml': ['node scripts/lint-staged-mjml.js'],
-	// TODO: re-generate all template if includes/* changes
-};
