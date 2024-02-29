9b94741df360c2b9e09409077a9393578b0d85fb
Quang Phan <vnphanquang@gmail.com>
2024-02-29 16:40:04 +0700

    refactor(sites/www): swap qr code in blue screen of death to use the qr service instead of dynamic @svelte-put/qr

--- sites/www/src/routes/(localized)/blue-screen-of-death/+page.server.ts
+++ sites/www/src/routes/(localized)/blue-screen-of-death/+page.server.ts
@@ -1,4 +1,7 @@
-import { LOAD_DEPENDENCIES } from '$lib/constants';
+import { createQrUrl } from '@internals/isc/qr';
+
+import { QR_JWT_SECRET, QR_SERVICE_URL } from '$env/static/private';
+import { LOAD_DEPENDENCIES, SOCIAL_LINKS } from '$lib/constants';
 import { prepareRoutePageData } from '$lib/routing/routing.server';
 
 import type { PageServerLoad } from './$types';
@@ -7,9 +10,14 @@ export const load: PageServerLoad = async ({ locals, depends }) => {
 	depends(LOAD_DEPENDENCIES.LANGUAGE);
 	return {
 		route: prepareRoutePageData(locals.settings.language, 'blueScreenOfDeath'),
+		discordQR: await createQrUrl(QR_SERVICE_URL, QR_JWT_SECRET, {
+			data: SOCIAL_LINKS.DISCORD,
+			colorScheme: 'dark',
+			size: 256,
+		}),
 		meta: {
 			title: 'Blue Screen of Death | Svelte Vietnam',
-			description: '',
+			description: 'Hey look, something familiar!',
 		},
 	};
 };
