ec5fcbb4342179f0a33f48cdcdcfe74d0c14d074
Quang Phan <vnphanquang@gmail.com>
2024-03-20 09:06:55 +0700

    refactor(sites/www): error page - show both code & message

--- sites/www/src/lib/errors/index.ts
+++ sites/www/src/lib/errors/index.ts
@@ -1,10 +1,10 @@
 import { WWW_ERRORS } from '@internals/isc/www';
 import { error, type NumericRange } from '@sveltejs/kit';
 
-export function throwSvelteKitError(key: keyof typeof WWW_ERRORS, message?: string): never {
+export function throwSvelteKitError(key: keyof typeof WWW_ERRORS, message = ''): never {
 	const e = WWW_ERRORS[key];
 	return error(e.status as NumericRange<400, 599>, {
 		code: e.code,
-		message: message ?? e.code,
+		message,
 	});
 }
--- sites/www/src/routes/+error.svelte
+++ sites/www/src/routes/+error.svelte
@@ -11,7 +11,7 @@
 <main class="grid h-screen w-screen place-items-center text-center">
 	<section>
 		<h1 class="c-text-h1 font-bold">{$page.status}</h1>
-		<p>Oops, {$page.error?.message}</p>
+		<p>Oops, [{$page.error?.code}] {$page.error?.message}</p>
 		<label class="mt-6 block" for="blue-screen-toggler">
 			<a class="c-link" href={ROUTE_MAP.blueScreenOfDeath[lang].path}
 				>Would you like a blue screen?</a
