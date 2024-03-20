c0975486f028c7816e2a0e1ddc9ffb6f71fabe31
Quang Phan <vnphanquang@gmail.com>
2024-03-20 09:04:12 +0700

    feat(sites/www): migration & dao setup for event_checkins table

---
+++ sites/www/d1/migrations/0001_create_event_checkins_table.sql
@@ -0,0 +1,12 @@
+-- Migration number: 0001 	 2024-03-19T23:46:17.667Z
+DROP TABLE IF EXISTS event_checkins;
+CREATE TABLE IF NOT EXISTS event_checkins
+(
+	event TEXT NOT NULL,
+	email TEXT NOT NULL,
+	method TEXT NOT NULL,
+	created_by TEXT NOT NULL,
+	created_at TEXT NOT NULL,
+
+	PRIMARY KEY (email, event)
+);
