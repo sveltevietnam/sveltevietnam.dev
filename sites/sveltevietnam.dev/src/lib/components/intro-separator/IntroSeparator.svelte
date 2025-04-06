febc307bf3695ab1427e5b283217f1e6dfe7eaa7
Quang Phan <vnphanquang@gmail.com>
2025-04-06 16:42:08 +0700

    style(sveltevietnam.dev): add a full variant to intro-separator component

--- sites/sveltevietnam.dev/src/lib/components/intro-separator/IntroSeparator.svelte
+++ sites/sveltevietnam.dev/src/lib/components/intro-separator/IntroSeparator.svelte
@@ -3,18 +3,23 @@
 
 	let {
 		class: cls,
+		variant,
 		...rest
 	}: HTMLAttributes<HTMLElement> & {
-		variant?: 'default';
+		variant?: 'default' | 'full';
 	} = $props();
 </script>
 
-<div class={['flex gap-3', cls]} {...rest}>
-	<div class="bg-primary h-6 w-6"></div>
-	<div class="bg-primary h-6 flex-1"></div>
-	<div class="bg-primary h-6 w-6"></div>
-	<div class="bg-primary h-6 w-6 rounded-full"></div>
-	<div class="bg-primary h-6 w-6"></div>
-	<div class="bg-primary h-6 flex-1"></div>
-	<div class="bg-primary h-6 w-6"></div>
-</div>
+{#if variant === 'full'}
+	<div class={['bg-primary h-6 w-full', cls]}></div>
+{:else}
+	<div class={['flex gap-3', cls]} {...rest}>
+		<div class="bg-primary h-6 w-6"></div>
+		<div class="bg-primary h-6 flex-1"></div>
+		<div class="bg-primary h-6 w-6"></div>
+		<div class="bg-primary h-6 w-6 rounded-full"></div>
+		<div class="bg-primary h-6 w-6"></div>
+		<div class="bg-primary h-6 flex-1"></div>
+		<div class="bg-primary h-6 w-6"></div>
+	</div>
+{/if}
