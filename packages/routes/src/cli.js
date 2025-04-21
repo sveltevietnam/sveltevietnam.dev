298f05e77a26d5ff87ab5de0ffd038ac23ea10f0
Quang Phan <vnphanquang@gmail.com>
2025-04-21 11:38:44 +0700

    chore(packages/routes): remove cli entry point

--- packages/routes/src/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { program } from 'commander';
-
-import pkg from '../package.json' with { type: 'json' };
-
-program.name('routes').description(pkg.description).version(pkg.version);
-
-program.parse();
