02d48ba0f716b45494e3cbb597e280ecc55b5a0a
Tia Nguyen <tia.nkm03@gmail.com>
2025-04-29 09:38:32 +0100

    [V1] refactor(sveltevietnam.dev): resolve comments on sponsor page

--- sites/sveltevietnam.dev/src/routes/(site)/[lang=lang]/sponsor/+page.svelte
+++ sites/sveltevietnam.dev/src/routes/(site)/[lang=lang]/sponsor/+page.svelte
@@ -1,35 +1,19 @@
 <script lang="ts">
 	import { T } from '@sveltevietnam/i18n';
-	import type { Message, MessageType } from '@sveltevietnam/i18n/runtime';
 
 	import { EMAILS } from '$data/emails';
 	import { SOCIAL_LINKS } from '$data/links';
 	import * as m from '$data/locales/generated/messages';
+	import * as p from '$data/routes/generated';
 	import { Breadcrumbs } from '$lib/components/breadcrumbs';
 	import { IntroSeparator } from '$lib/components/intro-separator';
 	import { RoutingContext } from '$lib/routing/context.svelte';
+	import { SettingsContext } from '$lib/settings/context.svelte';
 
 	import imgIntro from './_page/images/irrigation.svg?url';
 
+	const settings = SettingsContext.get();
 	const routing = RoutingContext.get();
-	const links: { message: Message<MessageType, never>; href: string }[] = [
-		{
-			message: m['pages.sponsor.participate.discord'],
-			href: SOCIAL_LINKS.DISCORD,
-		},
-		{
-			message: m['pages.sponsor.participate.events'],
-			href: `/${routing.paths.en ? 'en' : 'vi'}/events`,
-		},
-		{
-			message: m['pages.sponsor.participate.blog'],
-			href: `/${routing.paths.en ? 'en' : 'vi'}/blog`,
-		},
-		{
-			message: m['pages.sponsor.participate.github'],
-			href: SOCIAL_LINKS.GITHUB,
-		},
-	];
 </script>
 
 <main>
@@ -55,81 +39,110 @@
 				width="320"
 				height="320"
 				alt="a human watering the earth"
+				fetchpriority="high"
 			/>
 		</div>
 		<IntroSeparator />
 	</section>
 
-	<!-- Donate -->
-	<section class="space-y-section bg-gradient-secondary-intro py-section" id="donate">
-		<div class="max-w-pad flex flex-col gap-6">
-			<div class={['border-onehalf bg-surface drop-shadow-brutal relative space-y-2 px-10 py-6']}>
-				<h1 class="c-text-heading-lg border-b">Open Collective</h1>
-				<p class="my-6">
-					<T message={m['pages.sponsor.highlight.desc']} />
-				</p>
-				<a class="c-btn c-btn--pop block w-fit" href={SOCIAL_LINKS.OPEN_COLLECTIVE} target="_blank"
-					><T message={m['pages.sponsor.highlight.cta']} />
-					<i class="i i-[ph--arrow-square-out] h-6 w-6"></i></a
-				>
-				<p class="my-6">
-					<T message={m['pages.sponsor.highlight.contact']} />
-					<a class="c-link" href="mailto:{EMAILS.SPONSOR}">{EMAILS.SPONSOR}</a>.
-				</p>
-			</div>
+	<!-- How -->
+	<section class="py-section max-w-pad" id="how">
+		<div
+			class="border-onehalf bg-surface shadow-brutal-lg tablet:p-8 desktop:p-10 desktop:pt-6 tablet:pt-4 relative space-y-6 p-6 pt-4"
+		>
+			<h1 class="c-text-heading-lg border-b">Open Collective</h1>
+			<p class="leading-relaxed">
+				<T message={m['pages.sponsor.highlight.desc']} />
+			</p>
+			<a
+				class="c-btn c-btn--pop block w-fit"
+				href={SOCIAL_LINKS.OPEN_COLLECTIVE}
+				target="_blank"
+				rel="noopener noreferrer external"
+				><T message={m['pages.sponsor.highlight.cta']} />
+				<i class="i i-[ph--arrow-square-out] h-6 w-6"></i></a
+			>
+			<p>
+				<T message={m['pages.sponsor.highlight.contact']} />
+				<a class="c-link" href="mailto:{EMAILS.SPONSOR}" rel="noopener noreferrer"
+					>{EMAILS.SPONSOR}</a
+				>.
+			</p>
 		</div>
 	</section>
 
 	<!-- Participate -->
-	<section class="space-y-section py-section">
-		<div class="max-w-pad flex flex-col gap-6">
-			<h1 class="c-text-heading-lg border-b">
-				<T message={m['pages.sponsor.participate.heading']} />
-			</h1>
-			<p>
-				<T message={m['pages.sponsor.participate.desc']} />
-			</p>
-			<ul class="mobile:order-3 flex flex-col gap-6">
-				{#each links as { message, href } (href)}
-					<a class="c-btn c-btn--pop" {href} target="_blank">
-						<span><T {message} /></span>
-						<i class="i i-[ph--arrow-square-out] h-6 w-6"></i>
-					</a>
-				{/each}
-			</ul>
-		</div>
+	<section class="py-section max-w-pad space-y-6">
+		<h1 class="c-text-heading-lg border-b">
+			<T message={m['pages.sponsor.participate.heading']} />
+		</h1>
+		<p class="max-w-readable leading-relaxed">
+			<T message={m['pages.sponsor.participate.desc']} />
+		</p>
+		<ul class="max-w-readable flex flex-col gap-6">
+			<li>
+				<a
+					class="c-btn c-btn--pop flex justify-between"
+					href={SOCIAL_LINKS.DISCORD}
+					target="_blank"
+					rel="noopener noreferrer external"
+					><T message={m['pages.sponsor.participate.discord']} />
+					<i class="i i-[ph--arrow-square-out] h-6 w-6"></i></a
+				>
+			</li>
+			<li>
+				<a
+					class="c-btn c-btn--pop flex justify-between text-left"
+					href={p['/:lang/events']({ lang: settings.language })}
+					><T message={m['pages.sponsor.participate.events']} /></a
+				>
+			</li>
+			<li>
+				<a
+					class="c-btn c-btn--pop flex justify-between text-left"
+					href={p['/:lang/blog']({ lang: settings.language })}
+					><T message={m['pages.sponsor.participate.blog']} /></a
+				>
+			</li>
+			<li>
+				<a
+					class="c-btn c-btn--pop flex w-full items-center justify-between"
+					href={SOCIAL_LINKS.GITHUB}
+					target="_blank"
+					rel="noopener noreferrer external"
+					><T message={m['pages.sponsor.participate.github']} />
+					<i class="i i-[ph--arrow-square-out] h-6 w-6"></i></a
+				>
+			</li>
+		</ul>
 	</section>
 
 	<!-- Why -->
-	<section id="why">
-		<div class="max-w-pad bg-gradient-primary pt-section pb-section-more space-y-10">
-			<h1 class="c-text-heading-lg border-b"><T message={m['pages.sponsor.why.heading']} /></h1>
-			<p>
-				<T message={m['pages.sponsor.why.desc']} />
-				<br /><br />
-				<T message={m['pages.sponsor.why.reasons.opening']} />
-			</p>
-			<ul class="list-disc pl-6">
-				<li>
-					<T message={m['pages.sponsor.why.reasons.one']} />
-				</li>
-				<li>
-					<T message={m['pages.sponsor.why.reasons.two']} />
-				</li>
-			</ul>
-			<p>
-				<T message={m['pages.sponsor.why.reasons.closing']} />
-			</p>
-		</div>
+	<section class="max-w-pad bg-gradient-primary py-section space-y-6" id="why">
+		<h1 class="c-text-heading-lg border-b"><T message={m['pages.sponsor.why.heading']} /></h1>
+		<p class="leading-relaxed">
+			<T message={m['pages.sponsor.why.desc']} />
+			<br /><br />
+			<T message={m['pages.sponsor.why.reasons.opening']} />
+		</p>
+		<ul class="pl-6s list-disc">
+			<li>
+				<T message={m['pages.sponsor.why.reasons.one']} />
+			</li>
+			<li>
+				<T message={m['pages.sponsor.why.reasons.two']} />
+			</li>
+		</ul>
+		<p class="leading-relaxed">
+			<T message={m['pages.sponsor.why.reasons.closing']} />
+		</p>
 	</section>
 
 	<!-- Benefits -->
-	<section class="space-y-section py-section">
-		<div class="max-w-pad flex flex-col gap-6">
-			<h1 class="c-text-heading-lg border-b"><T message={m['pages.sponsor.benefit.heading']} /></h1>
-			<p>
-				<T message={m['pages.sponsor.benefit.desc']} />
-			</p>
-		</div>
+	<section class="py-section max-w-pad space-y-6">
+		<h1 class="c-text-heading-lg border-b"><T message={m['pages.sponsor.benefit.heading']} /></h1>
+		<p class="leading-relaxed">
+			<T message={m['pages.sponsor.benefit.desc']} />
+		</p>
 	</section>
 </main>
