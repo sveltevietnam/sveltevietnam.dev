192cabe1762b25d031c1f3eeab1110aad827dbce
Quang Phan <vnphanquang@gmail.com>
2025-05-08 22:41:55 +0700

    refactor(sveltevietnam.dev): self contain post series data in a single definition file

--- sites/sveltevietnam.dev/src/data/blog/series/styling-svelte-vietnam/index.ts
+++ sites/sveltevietnam.dev/src/data/blog/series/styling-svelte-vietnam/index.ts
@@ -1,9 +1,17 @@
 import { defineBlogSeries } from '..';
-import * as m from '../../locales/generated/messages';
 
 export default defineBlogSeries((lang) => ({
-	name: m['series.styling_svelte_vietnam.name'](lang),
-	slug: m['series.styling_svelte_vietnam.slug'](lang),
-	description: m['series.styling_svelte_vietnam.desc'](lang),
+	slug: 'styling-svelte-vietnam',
+	...{
+		en: {
+			name: 'Styling Svelte Vietnam',
+			description:
+				'A three-parted sub-series of "Behind the Screen" focusing on discussions about the design and usage of CSS system for sveltevietnam.dev',
+		},
+		vi: {
+			name: 'Styling cho Svelte Việt Nam',
+			description:
+				'Tập hợp ba bài viết trong chuỗi "Behind the Screen" với tập trung vào cách thiết kế hệ thống CSS cho trang web sveltevietnam.dev',
+		},
+	}[lang],
 }));
-
