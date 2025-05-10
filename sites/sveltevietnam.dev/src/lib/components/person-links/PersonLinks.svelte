1bacb913acd2544e0c9f7f5fa0e1884018f341bb
Quang Phan <vnphanquang@gmail.com>
2025-05-10 20:09:32 +0700

    style(sveltevietnam.dev): simplify icon matching - PersonLinks component

--- sites/sveltevietnam.dev/src/lib/components/person-links/PersonLinks.svelte
+++ sites/sveltevietnam.dev/src/lib/components/person-links/PersonLinks.svelte
@@ -10,19 +10,30 @@
 	} = $props();
 
 	let rLinks = $derived(links ? Object.entries(links) : []);
+
+	const icons = {
+		github: 'i-[simple-icons--github]',
+		twitter: 'i-[simple-icons--x]',
+		x: 'i-[simple-icons--x]',
+		linkedin: 'i-[simple-icons--linkedin]',
+		bluesky: 'i-[simple-icons--bluesky]',
+	} as Record<string, string>;
 </script>
 
 {#if rLinks.length}
 	<ul class={['flex flex-wrap items-center gap-4', cls]} {...rest}>
 		{#each rLinks as [name, href] (name)}
 			<li>
-				<a class="c-link-icon flex rounded-full border border-current p-2" {href} data-external>
-					{#if name === 'bluesky'}
-						<i class="i i-[simple-icons--bluesky] h-6 w-6"></i>
-					{:else if name === 'github'}
-						<i class="i i-[simple-icons--github] h-6 w-6"></i>
-					{:else if name === 'x'}
-						<i class="i i-[simple-icons--x] h-6 w-6"></i>
+				<a
+					class={[
+						'c-link-icon flex rounded-full border-onehalf border-current',
+						name in icons ? 'p-3' : 'p-2',
+					]}
+					{href}
+					data-external
+				>
+					{#if name in icons}
+						<i class="i {icons[name]} h-5 w-5"></i>
 					{:else}
 						<i class="i i-[ph--globe] h-6 w-6"></i>
 					{/if}
