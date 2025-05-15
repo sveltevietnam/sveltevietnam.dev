062939c62ba82d3a4d5b910bf6dd2211a7e3ef54
Quang Phan <vnphanquang@gmail.com>
2025-05-15 11:30:32 +0700

    fix(i18n): make it extra sure to rerender when params change (T.svelte)

--- packages/i18n/src/runtime/T.svelte
+++ packages/i18n/src/runtime/T.svelte
@@ -1,7 +1,4 @@
-<script
-	lang="ts"
-	generics="M extends import('./types.public.js').Message"
->
+<script lang="ts" generics="M extends import('./types.public.js').Message">
 	import { getContext } from 'svelte';
 
 	let {
@@ -14,16 +11,18 @@
 	} & Record<keyof ReturnType<M>['$p'], string> = $props();
 
 	let getLang = getContext<() => string>('t:lang');
-	let lang = $derived(langFromProp ?? getLang());
-	let iMessage = $derived(message(lang));
-
-	const params = rest as unknown as Record<keyof ReturnType<M>['$p'], string>;
+	let [iMessage, params] = $derived([
+		message(langFromProp ?? getLang()),
+		rest as unknown as Record<keyof ReturnType<M>['$p'], string>,
+	]);
 </script>
 
-{#if iMessage.$t === 'snippet'}
-	{@render iMessage(params)}
-{:else if iMessage.$t === 'function'}
-	{iMessage(params)}
-{:else}
-	{iMessage as unknown as string}
-{/if}
+{#key Object.values(params)}
+	{#if iMessage.$t === 'snippet'}
+		{@render iMessage(params)}
+	{:else if iMessage.$t === 'function'}
+		{iMessage(params)}
+	{:else}
+		{iMessage as unknown as string}
+	{/if}
+{/key}
