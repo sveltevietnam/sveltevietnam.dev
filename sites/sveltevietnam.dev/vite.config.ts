5230515da4331438c0673506dce92d5ba4903079
Quang Phan <vnphanquang@gmail.com>
2025-05-15 11:34:32 +0700

    chore: upgrade dependencies

--- sites/sveltevietnam.dev/vite.config.ts
+++ sites/sveltevietnam.dev/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
 		 * @see {@link https://github.com/sveltejs/kit/issues/11793}
 		 */
 		include: [
+			'lodash.debounce',
 			'@floating-ui/dom',
 			'@svelte-put/lockscroll',
 			'@svelte-put/toc',
