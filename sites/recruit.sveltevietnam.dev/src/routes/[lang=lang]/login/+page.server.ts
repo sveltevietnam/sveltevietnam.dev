8949af4220f82cc24b4ca1ad2d3f00d0ea9d5bdc
Quang Phan <vnphanquang@gmail.com>
2025-08-10 21:27:21 +0700

    feat(recruit): set up login page UI and form action

--- sites/recruit.sveltevietnam.dev/src/routes/[lang=lang]/login/+page.server.ts
+++ sites/recruit.sveltevietnam.dev/src/routes/[lang=lang]/login/+page.server.ts
@@ -1,11 +1,33 @@
+import { fail } from '@sveltejs/kit';
+import type { Language } from '@sveltevietnam/i18n';
+import { validateCloudflareToken } from '@sveltevietnam/kit/utilities';
+import { valibot } from 'sveltekit-superforms/adapters';
+import { superValidate, setError } from 'sveltekit-superforms/server';
+import * as v from 'valibot';
+
+import * as m from '$data/locales/generated/messages';
 import * as p from '$data/routes/generated';
 import * as b from '$data/routes/generated/breadcrumbs';
+import { VITE_PRIVATE_CLOUDFLARE_TURNSTILE_SECRET_KEY } from '$env/static/private';
 
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+
+function createLoginSchema(lang: Language) {
+	return v.object({
+		email: v.pipe(
+			v.string(),
+			v.nonEmpty(m['inputs.email.errors.nonempty'](lang)),
+			v.email(m['inputs.email.errors.invalid'](lang)),
+		),
+		turnstile: v.pipe(v.string(), v.nonEmpty(m['inputs.turnstile.errors.nonempty'](lang))),
+	});
+}
 
 export const load: PageServerLoad = async ({ params }) => {
 	const { lang } = params;
+	const schema = createLoginSchema(lang);
 	return {
+		form: await superValidate(valibot(schema)),
 		routing: {
 			breadcrumbs: b['/:lang/login']({ lang }),
 			paths: {
@@ -14,4 +36,34 @@ export const load: PageServerLoad = async ({ params }) => {
 			},
 		},
 	};
+};
+
+export const actions: Actions = {
+	login: async (event) => {
+		const { request, locals, getClientAddress } = event;
+
+		const schema = createLoginSchema(locals.language);
+		const form = await superValidate(request, valibot(schema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+		form.data.email = form.data.email.toLowerCase();
+
+		// check cloudflare turnstile captcha
+		const turnstile = await validateCloudflareToken(
+			VITE_PRIVATE_CLOUDFLARE_TURNSTILE_SECRET_KEY,
+			form.data.turnstile,
+			getClientAddress(),
+		);
+		if (!turnstile.success) {
+			setError(
+				form,
+				'turnstile',
+				turnstile.error?.[0] ?? m['inputs.turnstile.errors.unknown'](locals.language),
+			);
+			return fail(400, { form });
+		}
+
+		return { form };
+	},
 };
