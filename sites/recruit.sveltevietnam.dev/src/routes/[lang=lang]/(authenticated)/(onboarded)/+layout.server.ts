ff5a2f614c9a91b7e589070c7dda6099fa834d62
Quang Phan <vnphanquang@gmail.com>
2025-09-12 19:13:21 +0700

    feat(recruit): split authenicated routes further into onboarded and newcommer & onboarded, redirect accordingly

--- sites/recruit.sveltevietnam.dev/src/routes/[lang=lang]/(authenticated)/(onboarded)/+layout.server.ts
+++ sites/recruit.sveltevietnam.dev/src/routes/[lang=lang]/(authenticated)/(onboarded)/+layout.server.ts
@@ -2,9 +2,11 @@ import { redirect } from '@sveltejs/kit';
 
 import * as p from '$data/routes/generated';
 
-import type { LayoutServerLoad } from './$types';
+import type { LayoutServerLoad } from '../$types';
 
 export const load: LayoutServerLoad = async ({ locals, params }) => {
 	const { lang } = params;
-	if (!locals.user) redirect(302, p['/:lang/postings']({ lang }));
+	if (!locals.user?.onboardedAt) {
+		redirect(302, p['/:lang/onboarding']({ lang }));
+	}
 };
