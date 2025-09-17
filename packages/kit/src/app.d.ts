7dd02ea5c306aa07fcb300e659aefc22ba9c8594
Quang Phan <vnphanquang@gmail.com>
2025-09-17 18:47:48 +0700

    chore(kit): setup dev global App typings

--- packages/kit/src/app.d.ts
+++ packages/kit/src/app.d.ts
@@ -1,8 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 /// <reference types="vite/client" />
 
+import type { AppLocals, AppPlatform } from '@sveltevietnam/kit';
+
 declare global {
+	// @ts-expect-error
 	declare module '*&imagetools' {
+		// @ts-expect-error
 		export default import('vite-imagetools').Picture;
+	}
+
+	namespace App {
+		interface Locals extends AppLocals {}
+		interface Platform extends AppPlatform {}
 	}
 }
 
