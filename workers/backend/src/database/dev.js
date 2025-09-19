af5fa25bbc2cf5644983f0ef5cf4516541d46795
Quang Phan <vnphanquang@gmail.com>
2025-09-19 14:04:00 +0700

    feat(backend): export wrangler.json, schema, and utils to get orm & push schema programatically

--- workers/backend/src/database/dev.js
+++ workers/backend/src/database/dev.js
@@ -4,6 +4,7 @@
 
 import crypto from 'node:crypto';
 import fs from 'node:fs';
+import { createRequire } from 'node:module';
 import path from 'node:path';
 import sqlite from 'node:sqlite';
 
@@ -56,4 +57,59 @@ export function getLocalD1Path(databaseId, options = {}) {
 	}
 
 	return { path: filepath, created };
+}
+
+/**
+ * @template {Record<string, unknown>} TSchema
+ * @param {string} databaseId
+ * @param {TSchema} schema
+ * @returns {Promise<import('drizzle-orm/libsql').LibSQLDatabase<TSchema> & { $client: import('@libsql/client').Client }>}
+ */
+export async function getLocalORM(databaseId, schema) {
+	// 1. get local D1 path, create a vacuum database if not exist
+	const { path } = getLocalD1Path(databaseId);
+
+	// 2. create an explicit LibSQL client
+	const { createClient } = await import('@libsql/client');
+	const client = createClient({ url: `file:${path}` });
+
+	// 3. create a Drizzle instance
+	const { drizzle } = await import('drizzle-orm/libsql');
+	const orm = drizzle(client, { schema, casing: 'snake_case' });
+	orm.$client.close();
+
+	return orm;
+}
+
+/**
+ * https://github.com/drizzle-team/drizzle-orm/discussions/4373#discussioncomment-12743792
+ * @param {string} databaseId
+ * @param {Record<string, import('drizzle-orm/sqlite-core').SQLiteTable | import('drizzle-orm').Relations>} schema
+ * @returns {Promise<void>}
+ */
+export async function pushSchema(databaseId, schema) {
+	const orm = await getLocalORM(databaseId, schema);
+
+	// push schema
+	const require = createRequire(import.meta.url);
+	const { generateSQLiteDrizzleJson, generateSQLiteMigration } = require('drizzle-kit/api');
+	const prevJson = await generateSQLiteDrizzleJson({});
+	const curJson = await generateSQLiteDrizzleJson(schema, prevJson.id, 'snake_case');
+	const statements = await generateSQLiteMigration(prevJson, curJson);
+	for (const statement of statements) {
+		await orm.run(statement);
+	}
+
+	// cleanup
+	orm.$client.close();
+}
+
+/**
+ * @param {string} databaseId
+ */
+export function deleteLocalD1(databaseId) {
+	const { path } = getLocalD1Path(databaseId, { vacuum: false });
+	if (fs.existsSync(path)) {
+		fs.rmSync(path);
+	}
 }
