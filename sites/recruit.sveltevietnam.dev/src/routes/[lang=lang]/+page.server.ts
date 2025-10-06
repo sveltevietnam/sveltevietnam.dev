8bc58207a94b88c01410fba75069c58909303230
Quang Phan <vnphanquang@gmail.com>
2025-10-06 20:58:25 +0700

    chore(recruit): remove redirect from home -> postings when authenticated

--- sites/recruit.sveltevietnam.dev/src/routes/[lang=lang]/+page.server.ts
+++ sites/recruit.sveltevietnam.dev/src/routes/[lang=lang]/+page.server.ts
@@ -1,13 +1,9 @@
-import { redirect } from '@sveltejs/kit';
-
 import * as p from '$data/routes/generated';
 import * as b from '$data/routes/generated/breadcrumbs';
 
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ params, locals }) => {
-	if (locals.user) redirect(302, p['/:lang/postings']({ lang: params.lang }));
-
+export const load: PageServerLoad = ({ params }) => {
 	const { lang } = params;
 	return {
 		routing: {
