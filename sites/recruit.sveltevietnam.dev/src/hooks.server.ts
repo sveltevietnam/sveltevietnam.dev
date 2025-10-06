8b83df97013872d3ad1f20eddb28ab8f8d84d21e
Quang Phan <vnphanquang@gmail.com>
2025-10-06 20:57:40 +0700

    fix(recruit): move auth redirect check to hooks.server

--- sites/recruit.sveltevietnam.dev/src/hooks.server.ts
+++ sites/recruit.sveltevietnam.dev/src/hooks.server.ts
@@ -1,8 +1,10 @@
+import { redirect } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { createLangServerHook, createColorSchemeServerHook } from '@sveltevietnam/kit/hooks';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 import { building } from '$app/environment';
+import * as p from '$data/routes/generated';
 import { VITE_PRIVATE_COOKIE_NAME_LANGUAGE } from '$env/static/private';
 import { VITE_PUBLIC_COOKIE_NAME_COLOR_SCHEME, VITE_PUBLIC_MODE } from '$env/static/public';
 import { createEmployerAuth } from '$lib/auth';
@@ -10,6 +12,12 @@ import { createEmployerAuth } from '$lib/auth';
 const cookieDomain = VITE_PUBLIC_MODE === 'production' ? 'sveltevietnam.dev' : undefined;
 
 export const handle = sequence(
+	createLangServerHook({
+		cookie: {
+			name: VITE_PRIVATE_COOKIE_NAME_LANGUAGE,
+			domain: cookieDomain,
+		},
+	}),
 	async ({ event, resolve }) => {
 		// initialize auth
 		const auth = createEmployerAuth();
@@ -20,14 +28,15 @@ export const handle = sequence(
 			event.locals.session = session.session;
 			event.locals.user = session.user;
 		}
+		if (!event.locals.user && event.route.id?.includes('authenticated')) {
+			redirect(
+				302,
+				p['/:lang/authenticate']({ lang: event.locals.language }) +
+					`?callback=${event.url.pathname}`,
+			);
+		}
 		return svelteKitHandler({ event, resolve, auth, building });
 	},
-	createLangServerHook({
-		cookie: {
-			name: VITE_PRIVATE_COOKIE_NAME_LANGUAGE,
-			domain: cookieDomain,
-		},
-	}),
 	createColorSchemeServerHook({
 		building,
 		cookie: {
