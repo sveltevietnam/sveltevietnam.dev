492582301cef5d732202af78a889ab2dc65d0d8e
Quang Phan <vnphanquang@gmail.com>
2025-10-19 14:33:59 +0700

    refactor(i18n): rescope re-exports for utils subpackage

--- packages/i18n/src/language.js
+++ packages/i18n/src/language.js
@@ -2,10 +2,3 @@ export const LANGUAGES = /** @type {const} */ (['en', 'vi']);
 /** @typedef {(typeof LANGUAGES)[number]} Language */
 
 export const DEFAULT_LANGUAGE = /** @satisfies {Language}*/ ('en');
-
-/**
- * @template {string} [L = Language]
- * @template [V = string]
- * @typedef { V | Record<L, V>} LangVar
- */
-
