df755b03428a61fcf246e13c2f6e1adb2954e7c5
Quang Phan <vnphanquang@gmail.com>
2025-10-19 08:41:51 +0700

    chore(www): add authors, remove placeholders & empty lines, fix typo - quang-n people-of-svelte blog post

--- sites/sveltevietnam.dev/src/data/blog/posts/20251017-quang-n-blending-branding-svelte-and-ai-curiosity/metadata.ts
+++ sites/sveltevietnam.dev/src/data/blog/posts/20251017-quang-n-blending-branding-svelte-and-ai-curiosity/metadata.ts
@@ -2,10 +2,9 @@ import { defineBlogPostMetadata } from '..';
 
 export default defineBlogPostMetadata((lang) => ({
 	publishedAt: new Date('2025-10-17'),
-	authors: ['abc'],
+	authors: ['quang-design', 'liti-dev'],
 	categories: ['community'],
 	series: ['people-of-svelte'],
-	outdate: 0,
 	...(
 		{
 			en: {
@@ -15,7 +14,6 @@ export default defineBlogPostMetadata((lang) => ({
 					'Quang is a design engineer exploring the space between branding, Svelte, and AI. Calm on the outside but endlessly curious within, he’s blending design and technology to create something uniquely',
 				keywords: 'design, ai',
 				translation: 'original',
-				// TODO: update these information once you finish writing
 				readMinutes: 2,
 				numWords: 399,
 			},
@@ -26,7 +24,6 @@ export default defineBlogPostMetadata((lang) => ({
 					'Quang là một kĩ sư thiết kế với sở thích kết hợp giữa branding, Svelte và AI. Vẻ ngoài điềm tĩnh, luôn nung nấu nhiều ý tưởng. Quang thích kết hợp thiết kế và công nghệ để tạo nên điều gì đó đặc biệt.',
 				keywords: 'thiết kế, ai',
 				translation: 'manual',
-				// TODO: update these information once you finish writing
 				readMinutes: 2,
 				numWords: 399,
 			},
