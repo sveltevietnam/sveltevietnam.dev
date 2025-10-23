ab00578c501af8c148a1bed293c628919a193f37
Quang Phan <vnphanquang@gmail.com>
2025-10-23 15:38:23 +0700

    feat(i18n): add generateStringArrayConstant

---
+++ packages/i18n/src/here-be-dragon/compiler/generate-string-array-constant/generate-string-array-constant.js
@@ -0,0 +1,40 @@
+import ts, { factory } from 'typescript';
+
+// ===========
+// Public API
+// ===========
+/**
+ * @param {string} id
+ * @param {string[]} values
+ * @param {boolean} [sorted=false]
+ * @returns {ts.Node}
+ */
+export function generateStringArrayConstant(id, values, sorted) {
+	const expression = factory.createParenthesizedExpression(
+		factory.createArrayLiteralExpression(
+			(sorted ? values.toSorted() : values).map((value) => factory.createStringLiteral(value)),
+			false,
+		),
+	);
+	ts.addSyntheticLeadingComment(
+		expression,
+		ts.SyntaxKind.MultiLineCommentTrivia,
+		'*@const ',
+		false,
+	);
+	const node = factory.createVariableStatement(
+		[factory.createToken(ts.SyntaxKind.ExportKeyword)],
+		factory.createVariableDeclarationList(
+			[
+				factory.createVariableDeclaration(
+					factory.createIdentifier(id),
+					undefined,
+					undefined,
+					expression,
+				),
+			],
+			ts.NodeFlags.Const,
+		),
+	);
+	return node;
+}
