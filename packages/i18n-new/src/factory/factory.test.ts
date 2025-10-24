b6cda7d5779e9beb5d48195aaa5678e683037977
Quang Phan <vnphanquang@gmail.com>
2025-10-24 15:36:01 +0700

    refactor(i18n): move factory module out of runtime for clarity, fix up factory tests

--- packages/i18n-new/src/factory/factory.test.ts
+++ packages/i18n-new/src/factory/factory.test.ts
@@ -3,16 +3,19 @@ import { test, expect } from 'vitest';
 import { createMessageSimple, createMessageWithParams } from '.';
 
 test('can create simple message', () => {
-	const message = createMessageSimple('global.hello', () => 'Hello, world!');
+	const message = createMessageSimple('global.hello', {
+		vi: () => 'Xin chào!',
+		en: () => 'Hello, world!',
+	});
 	expect(message.$k).toBe('global.hello');
 	expect(message.$t).toBe('simple');
 });
 
 test('can create message with params', () => {
-	const message = createMessageWithParams(
-		'global.greet',
-		(params: { name: string }) => `Hello, ${params.name}!`,
-	);
+	const message = createMessageWithParams('global.greet', {
+		vi: (params: { name: string }) => `Xin chào, ${params.name}!`,
+		en: (params: { name: string }) => `Hello, ${params.name}!`,
+	});
 	expect(message.$k).toBe('global.greet');
 	expect(message.$t).toBe('with-params');
 });
