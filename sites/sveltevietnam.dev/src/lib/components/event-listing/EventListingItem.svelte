7a6acd840b3c3c6d4df799d292f48547e26dfe31
Quang Phan <vnphanquang@gmail.com>
2025-10-26 18:15:32 +0700

    refactor(www): simplify EventListingItem i18n with new implementation

--- sites/sveltevietnam.dev/src/lib/components/event-listing/EventListingItem.svelte
+++ sites/sveltevietnam.dev/src/lib/components/event-listing/EventListingItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" module>
-	import { T, type Message } from '@sveltevietnam/i18n/runtime';
+	import { T } from '@sveltevietnam/i18n-new';
+	import type { Key } from '@sveltevietnam/i18n-new/generated';
 	import fallback3x2 from '@sveltevietnam/kit/assets/images/fallbacks/3x2.jpg?enhanced&w=1200;700;400&imagetools';
 	import { CopyBtn } from '@sveltevietnam/kit/components';
 	import { Contexts } from '@sveltevietnam/kit/contexts';
@@ -11,7 +12,6 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 	import type { Picture } from 'vite-imagetools';
 
-	import * as m from '$data/locales/generated/messages';
 	import * as p from '$data/routes/generated';
 	import { SettingsContext } from '$lib/settings/context.svelte';
 
@@ -54,24 +54,24 @@
 
 <div class="@container">
 	<article
-		class="@xl:grid-cols-auto-2 @3xl:grid-cols-[auto_auto_1fr] @xl:gap-x-10 grid grid-cols-1 gap-4"
+		class="@xl:grid-cols-auto-2 grid grid-cols-1 gap-4 @xl:gap-x-10 @3xl:grid-cols-[auto_auto_1fr]"
 	>
 		<!-- date -->
 		<div
 			class={[
 				'flex h-fit items-start justify-between',
-				'@xl:items-end @xl:flex-col @xl:col-start-2 @xl:w-33',
-				'@3xl:items-start @3xl:col-start-1',
+				'@xl:col-start-2 @xl:w-33 @xl:flex-col @xl:items-end',
+				'@3xl:col-start-1 @3xl:items-start',
 			]}
 		>
 			{#if event.startDate instanceof Date}
 				<div
-					class={['grid-rows-auto-2 grid-cols-auto-2 @xl:gap-y-2 @xl:w-full grid w-fit gap-x-2']}
+					class={['grid-rows-auto-2 grid-cols-auto-2 grid w-fit gap-x-2 @xl:w-full @xl:gap-y-2']}
 				>
 					<p
 						class={[
-							'@xl:row-span-1 row-span-2',
-							'text-5xl font-bold leading-none tracking-widest',
+							'row-span-2 @xl:row-span-1',
+							'text-5xl leading-none font-bold tracking-widest',
 							'@xl:text-[4rem]',
 						]}
 					>
@@ -79,28 +79,28 @@
 					</p>
 					<p
 						class={[
-							'@xl:col-start-2 @xl:row-span-2 col-start-2',
+							'col-start-2 @xl:col-start-2 @xl:row-span-2',
 							'text-xl leading-tight tracking-wide',
-							'@xl:text-2xl @xl:[writing-mode:vertical-rl] @xl:tracking-normal',
+							'@xl:text-2xl @xl:tracking-normal @xl:[writing-mode:vertical-rl]',
 						]}
 					>
 						{formatLongMonth(routing.lang, event.startDate)}
 					</p>
 					<p
 						class={[
-							'@xl:col-start-1 col-start-2',
+							'col-start-2 @xl:col-start-1',
 							'text-on-surface-subtle leading-tight tracking-wide',
-							'@xl:leading-normal @xl:text-lg',
+							'@xl:text-lg @xl:leading-normal',
 						]}
 					>
 						{formatLongWeekDay(routing.lang, event.startDate)}
 					</p>
 				</div>
 			{:else}
-				<p class="text-5xl font-bold leading-none tracking-widest">TBA</p>
+				<p class="text-5xl leading-none font-bold tracking-widest">TBA</p>
 			{/if}
 
-			<div class="@xl:block bg-outline mb-6 mt-4 hidden h-1 w-full"></div>
+			<div class="bg-outline mt-4 mb-6 hidden h-1 w-full @xl:block"></div>
 
 			<!-- actions -->
 			<div>
@@ -121,9 +121,9 @@
 				href={path}
 				{...external ? { target: '_blank', rel: 'noreferrer noopner external' } : {}}
 			>
-				<span class="sr-only"><T message={m.view_more} /></span>
+				<span class="sr-only"><T key="view_more" /></span>
 				<enhanced:img
-					class="aspect-3/2 @3xl:max-w-72 h-auto"
+					class="aspect-3/2 h-auto @3xl:max-w-72"
 					src={img}
 					alt=""
 					fetchpriority="low"
@@ -135,8 +135,8 @@
 		</div>
 
 		<!-- content -->
-		<div class="@xl:col-span-2 @3xl:col-span-1 space-y-4">
-			<p class={['font-lora text-xl font-bold leading-normal', '@xl:text-2xl']}>
+		<div class="space-y-4 @xl:col-span-2 @3xl:col-span-1">
+			<p class={['font-lora text-xl leading-normal font-bold', '@xl:text-2xl']}>
 				<a
 					class="c-link-preserved relative"
 					href={path}
@@ -152,7 +152,7 @@
 					<div class="flex items-start gap-2">
 						<dt>
 							<i class="i i-[ph--map-pin] h-5 w-5"></i>
-							<span class="sr-only"><T message={m['location']} /></span>
+							<span class="sr-only"><T key="location" /></span>
 						</dt>
 						<dd>
 							<ListMessage items={event.location}>
@@ -183,7 +183,7 @@
 							<i class="i i-[ph--television] h-5 w-5"></i>
 						</dt>
 						<dd>
-							<span><T message={m['components.event_listing_item.live']} /></span>
+							<span><T key="components.event_listing_item.live" /></span>
 							<ListMessage items={event.livestream}>
 								{#snippet item(l)}
 									<a class="c-link" href={l.url}>
@@ -196,10 +196,10 @@
 				{/if}
 
 				<!-- time -->
-				{#snippet datetime(date: string | Date, prefix?: Message<'string', never>)}
+				{#snippet datetime(date: string | Date, prefix?: Key)}
 					{#if date instanceof Date}
 						{#if prefix}
-							<span class="capitalize"><T message={m['from']} /></span>
+							<span class="capitalize"><T key={prefix} /></span>
 						{/if}
 						{formatDateAndTime(date)}
 					{:else}
@@ -209,7 +209,7 @@
 				<div class="flex items-start gap-2">
 					<dt>
 						<i class="i i-[ph--clock] h-5 w-5"></i>
-						<span class="sr-only"><T message={m['time']} /></span>
+						<span class="sr-only"><T key="time" /></span>
 					</dt>
 					<dd>
 						{#if event.startDate && event.endDate}
@@ -225,9 +225,9 @@
 								{@render datetime(event.endDate)}
 							{/if}
 						{:else if event.startDate}
-							{@render datetime(event.startDate, m['from'])}
+							{@render datetime(event.startDate, 'from')}
 						{:else if event.endDate}
-							{@render datetime(event.endDate, m['to'])}
+							{@render datetime(event.endDate, 'to')}
 						{:else}
 							TBA
 						{/if}
