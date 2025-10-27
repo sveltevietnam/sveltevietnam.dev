5ba0da26ce1bceb7383e48f4e52fdb2227b55a78
Quang Phan <vnphanquang@gmail.com>
2025-10-27 20:46:17 +0700

    refactor(www): simplify i18n in roadmap page with new implementation

--- sites/sveltevietnam.dev/src/routes/(site)/[lang=lang]/roadmap/+page.svelte
+++ sites/sveltevietnam.dev/src/routes/(site)/[lang=lang]/roadmap/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { T } from '@sveltevietnam/i18n/runtime';
+	import { T } from '@sveltevietnam/i18n-new';
 	import { Breadcrumbs } from '@sveltevietnam/kit/components';
 
-	import * as m from '$data/locales/generated/messages';
 	import svgChimLac from '$lib/assets/images/svg/chim-lac.svg?no-inline';
 	import svgDong from '$lib/assets/images/svg/dong-thai-binh-hung-bao.svg?no-inline';
 	import svgTuChu from '$lib/assets/images/svg/tu-chu.svg?no-inline';
@@ -27,10 +26,10 @@
 				<Breadcrumbs crumbs={data.routing.breadcrumbs} />
 				<div class="space-y-4">
 					<h1 class="c-text-heading-page text-primary-on-surface">
-						<T message={m['pages.roadmap.heading']} />
+						<T key="pages.roadmap.heading" />
 					</h1>
 					<p class="c-text-subtitle-page max-w-readable-relaxed">
-						<T message={m['pages.roadmap.desc']} />
+						<T key="pages.roadmap.desc" />
 					</p>
 				</div>
 			</div>
@@ -70,63 +69,63 @@
 		<Milestone
 			id="dong"
 			name="Đồng"
-			objective={m['pages.roadmap.dong.objective']}
+			objective="pages.roadmap.dong.objective"
 			highlights={[
-				m['pages.roadmap.dong.highlights.v1'],
-				m['pages.roadmap.dong.highlights.people_of_svelte'],
-				m['pages.roadmap.dong.highlights.recruit'],
+				'pages.roadmap.dong.highlights.v1',
+				'pages.roadmap.dong.highlights.people_of_svelte',
+				'pages.roadmap.dong.highlights.recruit',
 			]}
 			tasks={[
 				{
-					message: m['pages.roadmap.dong.tasks.publish_packages'],
+					tKey: 'pages.roadmap.dong.tasks.publish_packages',
 					progress: 'inprogress',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.a11y'],
+					tKey: 'pages.roadmap.dong.tasks.a11y',
 					progress: 'inprogress',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.job_posting'],
+					tKey: 'pages.roadmap.dong.tasks.job_posting',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.umami'],
+					tKey: 'pages.roadmap.dong.tasks.umami',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.backend'],
+					tKey: 'pages.roadmap.dong.tasks.backend',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.people'],
+					tKey: 'pages.roadmap.dong.tasks.people',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.read_experience'],
+					tKey: 'pages.roadmap.dong.tasks.read_experience',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.write_guidelines'],
+					tKey: 'pages.roadmap.dong.tasks.write_guidelines',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.home_resources'],
+					tKey: 'pages.roadmap.dong.tasks.home_resources',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.svelte_5'],
+					tKey: 'pages.roadmap.dong.tasks.svelte_5',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.inter_4'],
+					tKey: 'pages.roadmap.dong.tasks.inter_4',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.tailwind_4'],
+					tKey: 'pages.roadmap.dong.tasks.tailwind_4',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.dong.tasks.refactor'],
+					tKey: 'pages.roadmap.dong.tasks.refactor',
 					progress: 'completed',
 				},
 			]}
@@ -150,52 +149,52 @@
 		<Milestone
 			id="tu"
 			name="Tự"
-			objective={m['pages.roadmap.tu.objective']}
+			objective="pages.roadmap.tu.objective"
 			highlights={[
-				m['pages.roadmap.tu.highlights.open_collective'],
-				m['pages.roadmap.tu.highlights.first_offline_meetup'],
-				m['pages.roadmap.tu.highlights.first_online_meetup'],
-				m['pages.roadmap.tu.highlights.first_ui'],
+				'pages.roadmap.tu.highlights.open_collective',
+				'pages.roadmap.tu.highlights.first_offline_meetup',
+				'pages.roadmap.tu.highlights.first_online_meetup',
+				'pages.roadmap.tu.highlights.first_ui',
 			]}
 			tasks={[
 				{
-					message: m['pages.roadmap.tu.tasks.survey'],
+					tKey: 'pages.roadmap.tu.tasks.survey',
 					progress: 'delayed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.page_jobs'],
+					tKey: 'pages.roadmap.tu.tasks.page_jobs',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.page_design'],
+					tKey: 'pages.roadmap.tu.tasks.page_design',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.progressive_enhancement'],
+					tKey: 'pages.roadmap.tu.tasks.progressive_enhancement',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.page_coc'],
+					tKey: 'pages.roadmap.tu.tasks.page_coc',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.og'],
+					tKey: 'pages.roadmap.tu.tasks.og',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.references'],
+					tKey: 'pages.roadmap.tu.tasks.references',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.page_blog'],
+					tKey: 'pages.roadmap.tu.tasks.page_blog',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.page_events'],
+					tKey: 'pages.roadmap.tu.tasks.page_events',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.tu.tasks.mailer'],
+					tKey: 'pages.roadmap.tu.tasks.mailer',
 					progress: 'completed',
 				},
 			]}
@@ -230,42 +229,42 @@
 		<Milestone
 			id="au-lac"
 			name="Âu Lạc"
-			objective={m['pages.roadmap.auLac.objective']}
+			objective="pages.roadmap.auLac.objective"
 			highlights={[
-				m['pages.roadmap.auLac.highlights.roadmap'],
-				m['pages.roadmap.auLac.highlights.initiation'],
+				'pages.roadmap.auLac.highlights.roadmap',
+				'pages.roadmap.auLac.highlights.initiation',
 			]}
 			tasks={[
 				{
-					message: m['pages.roadmap.auLac.tasks.page_roadmap'],
+					tKey: 'pages.roadmap.auLac.tasks.page_roadmap',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.metadata'],
+					tKey: 'pages.roadmap.auLac.tasks.metadata',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.discord'],
+					tKey: 'pages.roadmap.auLac.tasks.discord',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.page_home'],
+					tKey: 'pages.roadmap.auLac.tasks.page_home',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.page_branding_repo'],
+					tKey: 'pages.roadmap.auLac.tasks.page_branding_repo',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.logo'],
+					tKey: 'pages.roadmap.auLac.tasks.logo',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.project_reference'],
+					tKey: 'pages.roadmap.auLac.tasks.project_reference',
 					progress: 'completed',
 				},
 				{
-					message: m['pages.roadmap.auLac.tasks.github'],
+					tKey: 'pages.roadmap.auLac.tasks.github',
 					progress: 'completed',
 				},
 			]}
