2bc96aeed300b1836ef8cfcead6b6243bdc67096
Quang Phan <vnphanquang@gmail.com>
2025-10-28 18:08:06 +0700

    refactor(i18n+recruit): migrate i18n in SponsorReminder component to new implementation

--- sites/recruit.sveltevietnam.dev/src/lib/components/sponsor-reminder/SponsorReminder.svelte
+++ sites/recruit.sveltevietnam.dev/src/lib/components/sponsor-reminder/SponsorReminder.svelte
@@ -1,8 +1,6 @@
 <script lang="ts" module>
-	import { T } from '@sveltevietnam/i18n/runtime';
+	import { T } from '@sveltevietnam/i18n-new';
 	import type { HTMLAttributes } from 'svelte/elements';
-
-	import * as m from '$data/locales/generated/messages';
 
 	export type SponsorReminderProps = HTMLAttributes<HTMLElement>;
 </script>
@@ -14,20 +12,20 @@
 <section class={['space-y-6', cls]} {...rest}>
 	<div class=" border-outline flex items-end justify-between border-b">
 		<h2 class="c-text-heading">
-			<T message={m['components.sponsor_reminder.heading']} />
+			<T key="components.sponsor_reminder.heading" />
 		</h2>
 		<a
 			class="c-btn float-end inline-block"
 			href="https://opencollective.com/sveltevietnam"
 			data-umami-event="click-sponsor"
 		>
-			<T message={m['components.sponsor_reminder.cta']} />
+			<T key="components.sponsor_reminder.cta" />
 		</a>
 	</div>
 	<p class="leading-relaxed">
-		<T message={m['components.sponsor_reminder.desc']} />
+		<T key="components.sponsor_reminder.desc" />
 	</p>
 	<p class="c-callout c-callout--info c-callout--icon-bulb leading-relaxed">
-		<T message={m['components.sponsor_reminder.callout']} />
+		<T key="components.sponsor_reminder.callout" />
 	</p>
 </section>
