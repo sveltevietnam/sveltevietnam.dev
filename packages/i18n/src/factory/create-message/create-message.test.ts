d2d61acf63d86f0923624ca887bf577e8f58d3cc
Quang Phan <vnphanquang@gmail.com>
2025-10-31 00:55:11 +0700

    test(i18n): call actual message created by factory in test

--- packages/i18n/src/factory/create-message/create-message.test.ts
+++ packages/i18n/src/factory/create-message/create-message.test.ts
@@ -9,6 +9,8 @@ test('can create simple message', () => {
 	});
 	expect(message.$k).toBe('global.hello');
 	expect(message.$t).toBe('simple');
+	expect(message('vi')).toBe('Xin chào!');
+	expect(message('en')).toBe('Hello, world!');
 });
 
 test('can create message with params', () => {
@@ -18,4 +20,6 @@ test('can create message with params', () => {
 	});
 	expect(message.$k).toBe('global.greet');
 	expect(message.$t).toBe('with-params');
+	expect(message('vi', { name: 'An' })).toBe('Xin chào, An!');
+	expect(message('en', { name: 'An' })).toBe('Hello, An!');
 });
