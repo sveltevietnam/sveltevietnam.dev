a8c64e1703c0d05d2fc978176b6a6f590c6d36a7
Quang Phan <vnphanquang@gmail.com>
2025-10-31 18:45:40 +0700

    test(i18n): setup a separate project for vitest browser mode

--- packages/i18n/vitest.config.ts
+++ packages/i18n/vitest.config.ts
@@ -1,15 +1,40 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { playwright } from '@vitest/browser-playwright';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
-		include: ['**/*.test.ts', '**/test.ts'],
-		server: {
-			deps: {
-				inline: ['import-meta-resolve'],
+		projects: [
+			{
+				extends: true,
+				test: {
+					name: 'node',
+					environment: 'node',
+					include: ['**/*.test.ts', '**/test.ts'],
+					exclude: ['**/*.svelte.test.ts'],
+					server: {
+						deps: {
+							inline: ['import-meta-resolve'],
+						},
+					},
+				},
 			},
-		},
+			{
+				extends: true,
+				plugins: [svelte()],
+				test: {
+					name: 'browser',
+					include: ['**/*.svelte.test.ts'],
+					browser: {
+						provider: playwright(),
+						enabled: true,
+						instances: [{ browser: 'chromium' }],
+					},
+				},
+			},
+		],
 		coverage: {
 			provider: 'v8',
 		},
