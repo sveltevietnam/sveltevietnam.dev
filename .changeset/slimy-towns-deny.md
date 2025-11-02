9f7fb0a408f298a9edf5a1379dc1c2fb47264459
Quang Phan <vnphanquang@gmail.com>
2025-11-02 15:24:38 +0700

    fix(i18n): unite overload into one single interface for query function

---
+++ .changeset/slimy-towns-deny.md
@@ -0,0 +1,5 @@
+---
+'@sveltevietnam/i18n': patch
+---
+
+better type inference support for `query` remote function when imported from `@sveltevietnam/i18n/generated/t.remote`
--- packages/i18n/src/$generated/t.remote/index.d.ts
+++ packages/i18n/src/$generated/t.remote/index.d.ts
@@ -1,15 +1,17 @@
-import type { InferParams } from '@sveltevietnam/i18n';
-import type {
-	Language,
-	Mapping,
-	Key,
-	KeySimple,
-	KeyWithParams,
-} from '@sveltevietnam/i18n/generated';
+import type { InferParams, InferType, Message } from '@sveltevietnam/i18n';
+import type { Key, Language, Mapping } from '@sveltevietnam/i18n/generated';
 
-export function query<K extends KeySimple>(input: { key: K; lang: Language }): Promise<string>;
-export function query<K extends KeyWithParams>(input: {
-	key: K;
-	params: InferParams<Extract<Mapping[Key], { $k: K }>>;
-	lang: Language;
-}): Promise<string>;
+export function query<
+	M extends Record<string, Message> = Mapping,
+	L extends string = Language,
+	K extends keyof Mapping = Key,
+>(
+	input: {
+		key: K;
+		lang: L;
+	} & (InferType<M[K]> extends 'with-params'
+		? {
+				params: InferParams<M[K]>;
+			}
+		: Record<never, never>),
+): Promise<string>;
