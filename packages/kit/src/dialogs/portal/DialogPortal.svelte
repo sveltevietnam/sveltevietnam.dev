6cd435b33a1401bf1bb0147b94f776b4217468a1
Quang Phan <vnphanquang@gmail.com>
2025-11-04 22:44:26 +0700

    fix(www): turn off lockscroll on dialogs - workaround for current svelte & kit version

--- packages/kit/src/dialogs/portal/DialogPortal.svelte
+++ packages/kit/src/dialogs/portal/DialogPortal.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
-	import { afterNavigate } from '$app/navigation';
-
-	import { LockScrollContext } from '../../contexts/lockscroll.svelte.js';
-
 	import { type DialogPortalProps } from '.';
-
-	const lockscroll = LockScrollContext.get();
 
 	let { stack, class: cls, ...rest }: DialogPortalProps = $props();
 
-	// FIXME: workaround as of svelte@5.43.3, @sveltejs/kit@2.48.4
-	// attachment/action cleanup not called on navigation
-	afterNavigate(() => {
-		stack.items = [];
-	});
+	// FIXME: we're not locking scroll when dialog is opened for now
+	// as of svelte@5.43.3 & @sveltejs/kit@2.48.4, apply lockscroll does not release
+	// once user starts to type something into the search dialog, maybe something to do
+	// with the async work in there. Investigate later!
 </script>
 
 <aside class={['z-dialog pointer-events-none fixed inset-0', cls]} {...rest}>
 	{#each stack.items as dialog (dialog.config.id)}
-		<div
-			class="pointer-events-auto h-full w-full"
-			use:stack.actions.render={dialog}
-			{@attach lockscroll.lockWhileMounted()}
-		></div>
+		<div class="pointer-events-auto h-full w-full" use:stack.actions.render={dialog}></div>
 	{/each}
 </aside>
