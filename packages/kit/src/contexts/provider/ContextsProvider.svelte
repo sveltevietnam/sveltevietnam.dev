b4c2a0601e1a1203c7ce494bffbadd9b876c313b
Quang Phan <vnphanquang@gmail.com>
2025-11-05 18:02:34 +0700

    chore(kit): remove legacy t:lang context

--- packages/kit/src/contexts/provider/ContextsProvider.svelte
+++ packages/kit/src/contexts/provider/ContextsProvider.svelte
@@ -1,6 +1,4 @@
 <script lang="ts" module>
-	import { setContext } from 'svelte';
-
 	import { DialogPortal } from '../../dialogs';
 	import { NotificationPortal } from '../../notifications';
 
@@ -9,10 +7,6 @@
 
 <script lang="ts">
 	let { children, contexts }: ContextsProviderProps = $props();
-
-	// FIXME: extract to i18n package
-	// for @sveltevietnam/i18n T.svelte component
-	setContext('t:lang', () => contexts.routing.lang);
 </script>
 
 <svelte:document {@attach contexts.lockscroll.apply()} />
