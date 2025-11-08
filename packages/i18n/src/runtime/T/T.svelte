2b8007c7b3fee87b0a5a20fc9667a6423715e4c7
Quang Phan <vnphanquang@gmail.com>
2025-11-08 22:17:48 +0700

    Revert "fix(i18n): fix hydration mismatch issue in production build"
    This reverts commit 1f366dbb2f11fc12dfac71f986e302b59a49f5b6.

--- packages/i18n/src/runtime/T/T.svelte
+++ packages/i18n/src/runtime/T/T.svelte
@@ -1,8 +1,10 @@
 <script lang="ts" module>
 	import type { Key } from '@sveltevietnam/i18n/generated';
 
-	import { Context, type RemoteTranslate } from '../context';
+	import { Context } from '../context';
 	import type { Message } from '../types.public';
+
+	import MaybeHtml from './MaybeHtml.svelte';
 
 	import type { RemoteTProps, StaticTProps } from '.';
 </script>
@@ -23,13 +25,11 @@
 	if (!context) {
 		throw new Error("T component must live within a `import('@sveltevietnam/i18n').Context`");
 	}
-	let t = $derived(context.t as RemoteTranslate);
 
-	let translated = $derived<string>(
+	let maybePromise = $derived<string | Promise<string>>(
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		await t({ key, message, params, lang, sanitize, remote } as any),
+		(context.t as any)({ key, message, params, lang, sanitize, remote }),
 	);
 </script>
 
-<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-{@html translated}
+<MaybeHtml content={await maybePromise} />
