6e8ed6ef523a5ab75f92de9337b865d9bfe2e0ae
Quang Phan <vnphanquang@gmail.com>
2025-11-08 19:24:04 +0700

    chore(www): turn off BlogNewsletter temporarily on blog pages (pending migration to form remote function)

--- sites/sveltevietnam.dev/src/routes/(site)/[lang=lang]/blog/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { upsert } from '$lib/forms/subscriber/server';
-
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-	return {
-		subscribeFormData: await upsert.load(locals.language, 'blog'),
-	};
-};
